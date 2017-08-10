df8cfed157476cd05eb5eef265f0da89774258c3
Andrea Swick <andreamswick@gmail.com>
2017-08-09 21:50:19 -0500

    Set up events.index

--- app/Event.php
+++ app/Event.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = ['name', 'description', 'location', 'slug', 'start', 'end', 'open_at'];
+
+    public static function findBySlug($slug)
+    {
+        return self::where('slug', $slug)->firstOrFail();
+    }
 }
