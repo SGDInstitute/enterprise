39249c9d6a70521c96a1e50bddd4cf8be29e8022
Andrea Swick <andreamswick@gmail.com>
2017-08-11 14:50:46 -0500

    Add dates
    This will cast these fields as Carbon instances

--- app/Event.php
+++ app/Event.php
@@ -8,6 +8,8 @@ class Event extends Model
 {
     protected $fillable = ['name', 'description', 'location', 'slug', 'start', 'end', 'published_at'];
 
+    protected $dates = ['start', 'end', 'published_at'];
+
     public static function findBySlug($slug)
     {
         return self::where('slug', $slug)->firstOrFail();
