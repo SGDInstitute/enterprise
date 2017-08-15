616a54393e60971dac4aa1859c60ca673a7b558f
Andrea Swick <andreamswick@gmail.com>
2017-08-14 20:33:12 -0500

    Add is_open attribute

--- app/TicketType.php
+++ app/TicketType.php
@@ -2,12 +2,19 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class TicketType extends Model
 {
     public function getFormattedCostAttribute()
     {
-        return "$" . number_format($this->cost/100, 2);
+        return "$" . number_format($this->cost / 100, 2);
+    }
+
+    public function getIsOpenAttribute()
+    {
+        $now = Carbon::now();
+        return $this->availability_start < $now && $now < $this->availability_end;
     }
 }
--- tests/Unit/TicketTypeTest.php
+++ tests/Unit/TicketTypeTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\TicketType;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -16,5 +17,21 @@ class TicketTypeTest extends TestCase
         ]);
 
         $this->assertEquals('$100.00', $ticket->formatted_cost);
+    }
+
+    /** @test */
+    function can_view_if_open()
+    {
+        $openTicket = factory(TicketType::class)->make([
+            'availability_start' => Carbon::parse('-1 week'),
+            'availability_end' => Carbon::parse('+1 week')
+        ]);
+        $closedTicket = factory(TicketType::class)->make([
+            'availability_start' => Carbon::parse('+1 week'),
+            'availability_end' => Carbon::parse('+1 month')
+        ]);
+
+        $this->assertTrue($openTicket->is_open);
+        $this->assertFalse($closedTicket->is_open);
     }
 }
