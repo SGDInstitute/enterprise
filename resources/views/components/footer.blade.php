996ada730d57139dff4104ce3b858dc0d659e18d
Andrea Swick <andreamswick@gmail.com>
2017-08-14 21:23:16 -0500

    Remove twig filter

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,7 @@
                 <a href="https://sgdinstitute.org/"><img src="{{ asset('images/sgdinstitute-logo-white.png') }}" alt="Logo White" class="img-responsive"></a>
             </div>
             <div class="col-sm-8 visible-sm">
-                <p class="muted credit">&copy {{ "now"|date("Y") }} Midwest Institute for Sexuality and Gender Diversity</p>
+                <p class="muted credit">&copy {{ date("Y") }} Midwest Institute for Sexuality and Gender Diversity</p>
                 <ul class="list-inline copy-links">
                     <li><a href="/legal-privacy">Legal & Privacy</a></li>
                     <li><a href="mailto:webmaster@sgdinstitute.org">Webmaster</a></li>
@@ -18,7 +18,7 @@
             </div>
         </div>
         <div class="hidden-sm" style="font-size: 12px">
-            <p class="muted credit"><a href="/backend" class="copyright">&copy;</a> {{ "now"|date("Y") }} Midwest Institute for Sexuality and Gender Diversity</p>
+            <p class="muted credit"><a href="/backend" class="copyright">&copy;</a> {{ date("Y") }} Midwest Institute for Sexuality and Gender Diversity</p>
             <p class="muted credit">PO Box 1053, East Lansing, MI 48826-1053</p>
             <ul class="list-inline copy-links">
                 <li><a href="/legal-privacy">Legal & Privacy</a></li>
