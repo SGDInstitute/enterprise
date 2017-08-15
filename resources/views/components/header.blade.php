2dd801d5abe7612f040abaf8d401ffdb2980d617
Andrea Swick <andreamswick@gmail.com>
2017-08-14 23:35:14 -0500

    Update header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,22 +1,29 @@
 <header class="header">
-  <!-- Nav -->
-  <div class="container">
-    <nav id="layout-nav" class="navbar navbar-default" role="navigation">
-      <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">event.name</a>
-      </div>
-      <div class="collapse navbar-collapse navbar-main-collapse">
-          <ul class="nav navbar-nav navbar-right">
-              <li><a class="navbar-link" href="event.website">Event Website</a></li>
-              <li><a class="navbar-link" href="/login">login</a></li>
-          </ul>
-      </div>
+    <!-- Nav -->
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#navbar-collapse" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                @if($event)
+                    <a class="navbar-brand" href="/">{{ $event->title }}</a>
+                @endif
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    @if($event)
+                        <li><a class="navbar-link" target="_blank" href="{{ $event->links['website'] }}">Event
+                                Website</a></li>
+                    @endif
+                    <li><a class="navbar-link" href="/login">Login</a></li>
+                </ul>
+            </div>
+        </div>
     </nav>
-  </div>
 </header>
