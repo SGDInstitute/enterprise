4813c7d0cb6f838c980b17e9ea7add716dce1cf6
Matthew Dooley <Matthew@Dooley.xyz>
2017-08-15 22:45:15 -0500

    Changes navbar to fixed

--- resources/views/layouts/partials/app/header.blade.php
+++ resources/views/layouts/partials/app/header.blade.php
@@ -1,6 +1,6 @@
 <header class="header">
   <!-- Nav -->
-    <nav id="layout-nav" class="navbar navbar-default" role="navigation">
+    <nav id="layout-nav" class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
