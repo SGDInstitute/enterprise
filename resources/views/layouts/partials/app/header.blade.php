3191dfe8a0bce055898ed023f72ff05d102466c5
Matthew Dooley <Matthew@Dooley.xyz>
2017-08-15 21:37:21 -0500

    Moves components to paritials

---
+++ resources/views/layouts/partials/app/header.blade.php
@@ -0,0 +1,23 @@
+<header class="header">
+  <!-- Nav -->
+    <nav id="layout-nav" class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">event.name</a>
+        </div>
+        <div class="collapse navbar-collapse navbar-main-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a class="navbar-link" href="event.website">Event Website</a></li>
+                <li><a class="navbar-link" href="/login">login</a></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>
