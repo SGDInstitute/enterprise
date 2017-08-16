eaa69fb1f7a70e7f520b8a54e2a7db9b3c47b905
Matthew Dooley <Matthew@Dooley.xyz>
2017-08-15 21:38:02 -0500

    Updates for components -> partials change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,11 +22,11 @@
 </head>
 <body>
 <div id="app">
-    @include('components.header')
+    @include('layouts.partials.app.header')
 
     @yield('content')
 
-    @include('components.footer')
+    @include('layouts.partials.app.footer')
 </div>
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
