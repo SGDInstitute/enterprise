dc702e82bb981f0abea95061f9e050fd5c2ec8e6
Andrea Swick <andreamswick@gmail.com>
2017-08-17 09:49:32 -0500

    Add default navbar links

--- resources/views/layouts/partials/app/header.blade.php
+++ resources/views/layouts/partials/app/header.blade.php
@@ -21,7 +21,33 @@
                         <li><a class="navbar-link" target="_blank" href="{{ $event->links['website'] }}">Event
                                 Website</a></li>
                     @endif
-                    <li><a class="navbar-link" href="/login">Login</a></li>
+
+                    @guest
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                    @endguest
                 </ul>
             </div>
         </div>
