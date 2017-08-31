e32f40359d7ea431349cf308986781d88196d511
Andrea Swick <andreamswick@gmail.com>
2017-08-30 21:05:27 -0500

    Add home link to authenticated nav

--- resources/views/layouts/partials/app/header.blade.php
+++ resources/views/layouts/partials/app/header.blade.php
@@ -30,6 +30,7 @@
                         <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                     @else
+                        <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#"
                                id="navbarDropdownMenuLink" data-toggle="dropdown"
