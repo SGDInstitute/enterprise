677867419d95f9bba8ce7b3b8918e5b71a62d51c
Andrea Swick <andreamswick@gmail.com>
2017-09-06 13:59:12 -0500

    Compile to resources instead of public

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/admin.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/email.scss', 'public/css')
+    .sass('resources/assets/sass/email.scss', '../resources/assets/css')
     .sass('resources/assets/sass/admin.scss', 'public/css');
