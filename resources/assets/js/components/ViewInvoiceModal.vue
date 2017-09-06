5fee70383605c7a8132a396f62c33d4c4e8b4cc7
Andrea Swick <andreamswick@gmail.com>
2017-09-06 14:55:40 -0500

    Add spinner to invoice modal

--- resources/assets/js/components/ViewInvoiceModal.vue
+++ resources/assets/js/components/ViewInvoiceModal.vue
@@ -8,6 +8,9 @@
                             aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body">
+                    <div v-show="invoice === ''" class="text-center">
+                        <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>
+                    </div>
                     <div v-html="invoice"></div>
                 </div>
                 <div class="modal-footer">
