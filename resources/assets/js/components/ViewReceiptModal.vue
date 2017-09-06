c1b0ea4f52c319a06b28b3027d16213867117909
Andrea Swick <andreamswick@gmail.com>
2017-09-06 14:54:46 -0500

    Add text center to view receipt

--- resources/assets/js/components/ViewReceiptModal.vue
+++ resources/assets/js/components/ViewReceiptModal.vue
@@ -8,7 +8,7 @@
                             aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body">
-                    <div v-show="receipt === ''">
+                    <div v-show="receipt === ''" class="text-center">
                         <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>
                     </div>
                     <div v-html="receipt"></div>
