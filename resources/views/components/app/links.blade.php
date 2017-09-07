6fbe210aabf31e72fb967a751d833c56ec8e4882
Andrea Swick <andreamswick@gmail.com>
2017-09-07 10:54:04 -0500

    Fix wave errors for links

--- resources/views/components/app/links.blade.php
+++ resources/views/components/app/links.blade.php
@@ -2,11 +2,11 @@
     <div class="social-links {{ $class or '' }}">
         @foreach($links as $link)
             @if($link['icon'] === 'website')
-                <a href="{{ $link['link'] }}" target="_blank">
-                    <i class="fa fa-external-link" aria-label="External Link"></i>
+                <a href="{{ $link['link'] }}" target="_blank" aria-label="Website">
+                    <i class="fa fa-external-link" aria-label="Website"></i>
                 </a>
             @else
-                <a href="{{ $link['link'] }}" target="_blank">
+                <a href="{{ $link['link'] }}" target="_blank" aria-label="{{ $link['icon'] }}">
                     <i class="fa fa-{{ $link['icon'] }}" aria-label="{{ $link['icon'] }}"></i>
                 </a>
             @endif
