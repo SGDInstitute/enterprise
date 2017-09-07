d9f8db0e51bf1b25995579cc7fad432d1a5a2cd4
Andrea Swick <andreamswick@gmail.com>
2017-09-07 15:22:18 -0500

    Update profile factory

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -4,12 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Profile::class, function (Faker $faker) {
     return [
-        'pronouns' => 'he, him, his',
-        'sexuality' => 'Straight',
-        'gender' => 'Male',
-        'race' => 'White',
-        'college' => 'Hogwarts',
-        'tshirt' => 'M',
+        'pronouns' => array_random(["he", "she", "they"]),
+        'sexuality' => array_random(["Lesbian", "Gay", "Straight", "Ace"]),
+        'gender' => array_random(["Male", "Female", "GNC", "Trans"]),
+        'race' => $faker->colorName,
+        'college' => 'Illinois State',
+        'tshirt' => array_random(["S", "M", "L", "XL", "XXL"]),
         'accommodation' => $faker->paragraph(),
     ];
 });
