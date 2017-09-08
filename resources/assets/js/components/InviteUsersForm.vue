cbd238030ad8bc51b4a203c2b46de21ba3a5cec1
Andrea Swick <andreamswick@gmail.com>
2017-09-08 02:19:39 -0500

    Make form correctly formatted for sending to server

--- resources/assets/js/components/InviteUsersForm.vue
+++ resources/assets/js/components/InviteUsersForm.vue
@@ -14,11 +14,12 @@
                         you can fill as many or as little as you would like.</p>
 
                     <div class="form-group row" v-for="ticket in tickets">
-                        <label :for="'email' + index" class="col-md-4 col-form-label">Email for <small>#{{ ticket.hash }}</small></label>
+                        <label :for="ticket.hash" class="col-md-4 col-form-label">Email for <small>#{{ ticket.hash }}</small></label>
                         <div class="col-sm-8">
-                            <input type="email" class="form-control" :id="'email' + index" placeholder="Email">
+                            <input type="email" class="form-control" :id="ticket.hash" placeholder="Email" v-model="form.emails[ticket.hash]">
                         </div>
                     </div>
+                    
                     <div class="form-group">
                         <label for="message">Add a note to email:</label>
                         <textarea class="form-control" id="message" name="message" rows="3"></textarea>
@@ -39,13 +40,14 @@
         data() {
             return {
                 form: new SparkForm({
-                    emails: [],
+                    emails: {},
                     message: '',
                 }),
             }
         },
-        created() {
-            let self = this;
+        mounted() {
+            this.formFill();
+
             this.eventHub.$on('showInviteUsers', function () {
                 $('#inviteUsers').modal('show');
             });
@@ -53,6 +55,12 @@
         methods: {
             submit() {
 
+            },
+            formFill() {
+                let self = this;
+                _.forEach(this.tickets, function(ticket) {
+                    self.$set(self.form.emails, ticket.hash, '')
+                });
             }
         }
     }
