03debfadd2d70eecf316e09942c371b5ac5d85a1
Andrea Swick <andreamswick@gmail.com>
2017-09-08 01:26:05 -0500

    Remove unneeded test

--- tests/Feature/AddUsersToTicketsTest.php
+++ tests/Feature/AddUsersToTicketsTest.php
@@ -12,20 +12,4 @@ class AddUsersToTicketsTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
-    function can_view_edit_page()
-    {
-        $event = factory(Event::class)->states('published')->create();
-        $ticketType = $event->ticket_types()->save(factory(TicketType::class)->make());
-        $user = factory(User::class)->create(['email' => 'jo@example.com']);
-        $order = $event->orderTickets($user, [
-            ['ticket_type_id' => $ticketType->id, 'quantity' => 2]
-        ]);
-
-        $response = $this->withoutExceptionHandling()->actingAs($user)
-            ->get("/orders/{$order->id}/tickets");
-
-        $response->assertStatus(200)
-            ->assertViewHas('tickets');
-    }
 }
