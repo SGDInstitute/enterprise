3805ff5302834ba69b4177cf9035fb11c929349d
Andrea Swick <andreamswick@gmail.com>
2017-09-08 19:30:04 -0500

    Fix TicketTypeTest

--- tests/Unit/TicketTypeTest.php
+++ tests/Unit/TicketTypeTest.php
@@ -13,6 +13,7 @@ class TicketTypeTest extends TestCase
     function can_view_formatted_cost()
     {
         $ticket = factory(TicketType::class)->make([
+            'event_id' => 1,
             'cost' => 10000
         ]);
 
@@ -23,14 +24,17 @@ class TicketTypeTest extends TestCase
     function can_view_if_open()
     {
         $openTicket = factory(TicketType::class)->make([
+            'event_id' => 1,
             'availability_start' => Carbon::parse('-1 week'),
             'availability_end' => Carbon::parse('+1 week')
         ]);
         $closedTicket = factory(TicketType::class)->make([
+            'event_id' => 1,
             'availability_start' => Carbon::parse('+1 week'),
             'availability_end' => Carbon::parse('+1 month')
         ]);
         $alwaysOpenTicket = factory(TicketType::class)->make([
+            'event_id' => 1,
             'availability_start' => null,
             'availability_end' => null
         ]);
