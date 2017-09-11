5bc9091e787498ec876981f2279c1ee82cfa812f
Andrea Swick <andreamswick@gmail.com>
2017-09-10 21:33:26 -0500

    Add more style/information to invite email

--- resources/views/emails/invite.inky.php
+++ resources/views/emails/invite.inky.php
@@ -1,15 +1,34 @@
 @extends('layouts.email')
 
+@section('header')
+<div class="text-right">
+    @if(isset($ticket->order->event->logo_dark))
+    <img src="{{ $ticket->order->event->logo_dark }}" alt="{{ $ticket->order->event->title }} Logo"
+         style="width: 50%; float: right;">
+    @else
+    <h4 class="card-title">{{ $ticket->order->event->title }}</h4>
+    @endif
+</div>
+@endsection
+
 @section('content')
 <h2>Hi!</h2>
 
 <p>{{ $coordinator->name }} invited you to {{ $ticket->order->event->title }}!</p>
 
-@if($note)
+@if($note !== null)
 <p>They said to pass this along:</p>
 
-<p>{{ $note }}</p>
+<callout class="primary">
+    <p>{{ $note }}</p>
+</callout>
 @endif
 
-<a href="{{ $url }}">Set Your Account Password</a>
+<p>Click below to submit your personal information so you can receive your personalized name tag and a t-shirt at the conference!</p>
+
+<button href="{{ $url }}" class="radius text-center">Set Your Account Password</button>
+
+<p>Looking forward to seeing you,
+    <br>
+    The {{ $ticket->order->event->title }} Team
 @endsection
