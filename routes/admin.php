2c08c75457767e386753afe179555cdd405fc986
Andrea Swick <andreamswick@gmail.com>
2017-09-14 22:15:43 -0500

    Add event index and event show

--- routes/admin.php
+++ routes/admin.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::get('/admin', 'AdminController@index');
+
+Route::get('/admin/events', 'Admin\EventsController@index');
+Route::get('/admin/events/{slug}', 'Admin\EventsController@show');
