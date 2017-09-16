fdc43752a605f70b2074137dfee79422deab5bb1
Andrea Swick <andreamswick@gmail.com>
2017-09-15 20:04:50 -0500

    Reorder Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(EventsSeeder::class);
-        $this->call(DeveloperSeeder::class);
         $this->call(RolesPermissionsSeeder::class);
+        $this->call(DeveloperSeeder::class);
     }
 }
