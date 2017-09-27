48050712746a3a8b1d84924fd9ef33b6bcaebf53
Andrea Swick <andreamswick@gmail.com>
2017-09-27 16:30:02 -0500

    Add profile for user if they don’t already for settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Profile;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
     public function edit()
     {
+        $user = request()->user();
+        if(is_null(request()->user()->profile)) {
+            $user->profile()->save(new Profile());
+            $user->fresh();
+        }
+
         return view('settings.edit', [
-            'user' => request()->user()
+            'user' => $user
         ]);
     }
 }
