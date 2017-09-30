dc70d997dd89ec0eb5c1728e709271a1fde9bb18
Andrea Swick <andreamswick@gmail.com>
2017-09-30 16:59:27 -0500

    Start subscription test

---
+++ tests/Feature/SubscriptionDonationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentGateway;
+use App\Donation;
+use App\Mail\DonationEmail;
+use App\User;
+use Illuminate\Support\Facades\Mail;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubscriptionDonationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public $paymentGateway;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
+    /** @test */
+    function user_can_make_one_subscription_donation()
+    {
+        Mail::fake();
+
+        $user = factory(User::class)->create([
+            'name' => 'Harry Potter',
+            'email' => 'hpotter@hogwarts.edu',
+        ]);
+
+        $response = $this->withoutExceptionHandling()
+            ->actingAs($user)->json("post", "/donations", [
+                'amount' => 25,
+                'name' => 'Harry Potter',
+                'email' => 'hpotter@hogwarts.edu',
+                'subscription' => 'monthly',
+                'stripeToken' => $this->paymentGateway->getValidTestToken(),
+            ]);
+
+        $response->assertStatus(201);
+
+        $donation = Donation::where('email', 'hpotter@hogwarts.edu')->first();
+
+        $this->assertNotNull($donation);
+        $this->assertEquals(2500, $this->paymentGateway->totalSubscriptions());
+        $this->assertNotNull($donation->hash);
+        $this->assertEquals(2500, $donation->amount);
+        $this->assertEquals('Harry Potter', $donation->name);
+        $this->assertEquals('hpotter@hogwarts.edu', $donation->email);
+        $this->assertEquals('monthly-25', $donation->subscription->plan);
+        $this->assertTrue($donation->subscription->active);
+        $this->assertNotNull($donation->subscription->subscription_id);
+        $this->assertNotNull($donation->receipt->transaction_id);
+        $this->assertEquals($user->id, $donation->user_id);
+
+        Mail::assertSent(DonationEmail::class, function ($mail) use ($donation) {
+            return $mail->hasTo('hpotter@hogwarts.edu')
+                && $mail->donation->id === $donation->id;
+        });
+    }
+}
