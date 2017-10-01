291bc7556e427e8cdcfda60604c27ecaa5613935
Andrea Swick <andreamswick@gmail.com>
2017-10-01 09:36:24 -0500

    Start working on getting Fake and Stripe Payment Gateways to equal

--- tests/Unit/Billing/FakePaymentGatewayTest.php
+++ tests/Unit/Billing/FakePaymentGatewayTest.php
@@ -17,46 +17,4 @@ class FakePaymentGatewayTest extends TestCase
         return new FakePaymentGateway;
     }
 
-    /** @test */
-    function subscriptions_with_a_valid_payment_token_are_successful()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-
-        $newSubscriptions = $paymentGateway->newChargesDuring(function ($paymentGateway) {
-            $paymentGateway->subscribe('monthly-25', $paymentGateway->getValidTestCustomer());
-        });
-
-        $this->assertCount(1, $newSubscriptions);
-        $this->assertEquals('monthly-25', $newSubscriptions[0]['plan']);
-    }
-
-    /** @test */
-    function subscriptions_with_an_invalid_payment_token_fail()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-
-        $newCharges = $paymentGateway->newChargesDuring(function ($paymentGateway) {
-            try {
-                $paymentGateway->subscribe('monthly-25', 'invalid-customer');
-            } catch (PaymentFailedException $e) {
-                return;
-            }
-
-            $this->fail('Payment did not fail with an invalid payment token');
-        });
-
-        $this->assertCount(0, $newCharges);
-    }
-
-    /** @test */
-    function subscription_returns_object_with_id_amount_and_card_last_four()
-    {
-        $paymentGateway = $this->getPaymentGateway();
-
-        $subscription = $paymentGateway->subscribe('monthly-25', $paymentGateway->getValidTestCustomer());
-
-        $this->assertEquals('monthly-25', $subscription->get('plan'));
-        $this->assertNotNull($subscription->get('id'));
-        $this->assertNotNull($subscription->get('last4'));
-    }
 }
--- tests/Unit/Billing/PaymentGatewayContractTests.php
+++ tests/Unit/Billing/PaymentGatewayContractTests.php
@@ -78,4 +78,47 @@ trait PaymentGatewayContractTests
         $this->assertNotNull($charge->get('id'));
         $this->assertNotNull($charge->get('last4'));
     }
+
+    /** @test */
+    function subscriptions_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = $this->getPaymentGateway();
+
+        $newSubscriptions = $paymentGateway->newChargesDuring(function ($paymentGateway) {
+            $paymentGateway->subscribe('monthly-25', $paymentGateway->getValidTestCustomer());
+        });
+
+        $this->assertCount(1, $newSubscriptions);
+        $this->assertEquals('monthly-25', $newSubscriptions[0]['plan']);
+    }
+
+    /** @test */
+    function subscriptions_with_an_invalid_payment_token_fail()
+    {
+        $paymentGateway = $this->getPaymentGateway();
+
+        $newCharges = $paymentGateway->newChargesDuring(function ($paymentGateway) {
+            try {
+                $paymentGateway->subscribe('monthly-25', 'invalid-customer');
+            } catch (PaymentFailedException $e) {
+                return;
+            }
+
+            $this->fail('Payment did not fail with an invalid payment token');
+        });
+
+        $this->assertCount(0, $newCharges);
+    }
+
+    /** @test */
+    function subscription_returns_object_with_id_plan_and_card_last_four()
+    {
+        $paymentGateway = $this->getPaymentGateway();
+
+        $subscription = $paymentGateway->subscribe('monthly-25', $paymentGateway->getValidTestCustomer());
+
+        $this->assertEquals('monthly-25', $subscription->get('plan'));
+        $this->assertNotNull($subscription->get('id'));
+        $this->assertNotNull($subscription->get('last4'));
+    }
 }
