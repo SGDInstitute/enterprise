1dc48a22a1a6fadabcae8f3418e8ac3e108ec927
Andrea Swick <andreamswick@gmail.com>
2017-10-01 21:04:16 -0500

    Composer update and add back HasRoles trait

--- app/User.php
+++ app/User.php
@@ -10,11 +10,12 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Mail;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Permission\Traits\HasRoles;
 use Stripe\Customer;
 
 class User extends Authenticatable
 {
-    use Notifiable, LogsActivity, SoftDeletes;
+    use Notifiable, LogsActivity, SoftDeletes, HasRoles;
 
     /**
      * The attributes that are mass assignable.
