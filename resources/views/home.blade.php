db157bbbf3641339b415b7556140c06f75415461
Andrea Swick <andreamswick@gmail.com>
2017-10-02 09:01:11 -0500

    Add vue tabs to fix the weird bootstrap active state with multiple tabs on a page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,8 @@
                 <div class="nav list-group" id="v-pills-tab" role="tablist">
                     <a class="list-group-item list-group-item-action active" id="v-pills-orders-tab" data-toggle="pill"
                        href="#v-pills-orders" role="tab" aria-controls="v-pills-orders" aria-expanded="true">Orders</a>
-                    {{--<a class="list-group-item list-group-item-action" id="v-pills-donations-tab" data-toggle="pill"--}}
-                       {{--href="#v-pills-donations" role="tab" aria-controls="v-pills-donations" aria-expanded="true">Donations</a>--}}
-                    {{--<a class="list-group-item list-group-item-action" id="v-pills-workshops-tab" data-toggle="pill"--}}
-                       {{--href="#v-pills-workshops" role="tab" aria-controls="v-pills-workshops" aria-expanded="true">Workshops</a>--}}
-                    {{--<a class="list-group-item list-group-item-action" id="v-pills-volunteering-tab" data-toggle="pill"--}}
-                       {{--href="#v-pills-volunteering" role="tab" aria-controls="v-pills-volunteering"--}}
-                       {{--aria-expanded="true">Volunteering</a>--}}
+                    <a class="list-group-item list-group-item-action" id="v-pills-donations-tab" data-toggle="pill"
+                       href="#v-pills-donations" role="tab" aria-controls="v-pills-donations" aria-expanded="true">Donations</a>
                 </div>
             </div>
             <div class="col">
@@ -25,13 +20,8 @@
                         @include('home.partials.orders')
                     </div>
                     <div class="tab-pane fade" id="v-pills-donations" role="tabpanel"
-                         aria-labelledby="v-pills-donations-tab">...
-                    </div>
-                    <div class="tab-pane fade" id="v-pills-workshops" role="tabpanel"
-                         aria-labelledby="v-pills-workshops-tab">...
-                    </div>
-                    <div class="tab-pane fade" id="v-pills-volunteering" role="tabpanel"
-                         aria-labelledby="v-pills-volunteering-tab">...
+                         aria-labelledby="v-pills-donations-tab">
+                        @include('home.partials.donations')
                     </div>
                 </div>
             </div>
