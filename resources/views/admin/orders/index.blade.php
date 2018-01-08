c2d668f1129f809ffb441cd4fa835826f192065b
Andrea Swick <andreamswick@gmail.com>
2018-01-07 19:18:17 -0600

    Add information to orders list

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -14,8 +14,11 @@
                             <tr>
                                 <th>Created At</th>
                                 <th>User</th>
+                                <th>Email</th>
                                 <th>Is Paid</th>
                                 <th>Amount</th>
+                                <th># Tickets</th>
+                                <th># Tickets Filled</th>
                                 <th></th>
                             </tr>
                             </thead>
@@ -23,9 +26,12 @@
                             @foreach($orders as $order)
                                 <tr>
                                     <td>{{ $order->created_at->toFormattedDateString() }}</td>
-                                    <td>{{ $order->user->name }}</td>
+                                    <td><a href="{{ url('/admin/users/' . $order->user->id) }}">{{ $order->user->name }}</a></td>
+                                    <td><a href="mailto:{{ $order->user->email }}">{{ $order->user->email }}</a></td>
                                     <td>{{ $order->isPaid() ? 'Yes' : 'No' }}</td>
                                     <td>${{ number_format($order->amount/100, 2) }}</td>
+                                    <td>{{ $order->tickets->count() }}</td>
+                                    <td>{{ $order->tickets()->filled()->count()/$order->tickets->count()*100 }}% ({{ $order->tickets()->filled()->count() }})</td>
                                     <td class="text-right"><a href="/admin/orders/{{ $order->id }}"
                                                               class="btn btn-default btn-sm">View Order</a></td>
                                 </tr>
