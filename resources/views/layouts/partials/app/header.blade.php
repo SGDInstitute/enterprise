73d379b8aa787a10e830dff80e0beb32a9cab22f
Andrea Swick <andreamswick@gmail.com>
2018-01-17 08:41:12 -0600

    Add admin to header

--- resources/views/layouts/partials/app/header.blade.php
+++ resources/views/layouts/partials/app/header.blade.php
@@ -30,6 +30,9 @@
                         <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Create an Account</a></li>
                     @else
+                        @can('view_dashboard')
+                            <li class="nav-item"><a class="nav-link" href="/admin">Admin</a></li>
+                        @endcan
                         <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#"
