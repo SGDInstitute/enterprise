95a95289ce1db3062074a2bb488383936563ae3d
Andy Swick <andreamswick@gmail.com>
2018-09-12 09:36:53 -0500

    Fix emails until we can remove inky

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ let mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/admin.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/email.scss', '../resources/css')
+    .sass('resources/sass/email.scss', '../resources/assets/css')
     .sass('resources/sass/admin.scss', 'public/css');
 
 if (mix.inProduction()) {
