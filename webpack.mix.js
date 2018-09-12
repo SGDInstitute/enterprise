ad5b6e9be4c57374056161d5c24c099fff544db4
Laravel Shift <shift@laravelshift.com>
2018-09-12 13:35:52 +0000

    Move assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,11 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .js('resources/assets/js/admin.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/email.scss', '../resources/assets/css')
-    .sass('resources/assets/sass/admin.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/admin.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/email.scss', '../resources/css')
+    .sass('resources/sass/admin.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
