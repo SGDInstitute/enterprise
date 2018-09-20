370dc0bfffc1985adebcc776a163af9b8733e33e
Andy Swick <andreamswick@gmail.com>
2018-09-19 22:54:33 -0500

    Organize orders

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-
         return view('home', [
             'orders' => [
                 'upcoming' => request()->user()->upcomingOrdersAndTickets(),
