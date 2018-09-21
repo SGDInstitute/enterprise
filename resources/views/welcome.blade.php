78bfbdd7a141e4f481d3c28ff8ccd1bc7af89b65
Andy Swick <andreamswick@gmail.com>
2018-09-20 22:16:30 -0500

    Add padding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <h2 class="text-center">Upcoming Events</h2>
+                <h2 class="text-center mb-8">Upcoming Events</h2>
 
-                <div class="row">
+                <div class="row mb-8">
                     @foreach($upcomingEvents as $upcoming)
                         <div class="col-md-6">
                             @include("components.app.event", ['event' => $upcoming])
