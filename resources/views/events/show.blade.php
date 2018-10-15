7d2d1d7608dfb9c2b12d2a7c90f001288c666037
Andy Swick <andreamswick@gmail.com>
2018-10-15 09:42:06 -0500

    Add code for inclusion to show page and add margin bottom

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -53,7 +53,7 @@
                         Refund Policy <i class="fa fa-info-circle" aria-hidden="true"></i>
                     </a>
                 </p>
-                <div class="collapse" id="refund_policy">
+                <div class="collapse mb-4" id="refund_policy">
                     {!! $event->refund_policy !!}
                 </div>
                 @endif
@@ -64,8 +64,19 @@
                             Photo Policy <i class="fa fa-info-circle" aria-hidden="true"></i>
                         </a>
                     </p>
-                    <div class="collapse" id="photo_policy">
+                    <div class="collapse mb-4" id="photo_policy">
                         {!! $event->photo_policy !!}
+                    </div>
+                @endif
+
+                @if($event->inclusion_policy)
+                    <p>
+                        <a data-toggle="collapse" href="#inclusion_policy" role="button" aria-expanded="false" aria-controls="inclusion_policy">
+                            Code for Inclusion <i class="fa fa-info-circle" aria-hidden="true"></i>
+                        </a>
+                    </p>
+                    <div class="collapse mb-4" id="inclusion_policy">
+                        {!! $event->inclusion_policy !!}
                     </div>
                 @endif
             </div>
