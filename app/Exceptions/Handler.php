50a670e114a5af36ecbc2de634c3b73a11cf8603
Andy Swick <andreamswick@gmail.com>
2018-11-10 20:37:12 -0600

    Fix exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,8 +36,8 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        if (app()->bound('sentry') && $this->shouldReport($e)) {
-            app('sentry')->captureException($e);
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
         }
 
         parent::report($exception);
