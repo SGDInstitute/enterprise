8be547b289305cfcf92cb79ac62aa22995bb4538
Andy Swick <andreamswick@gmail.com>
2018-11-11 20:05:07 -0600

    Fix for array to string conversion

--- resources/views/exports/voyager/responses.blade.php
+++ resources/views/exports/voyager/responses.blade.php
@@ -10,7 +10,19 @@
     @foreach($responses as $response)
     <tr>
         @foreach($form->form as $question)
+            @if(is_array($response->responses[$question['id']]))
+            <td>
+                @foreach($response->responses[$question['id']] as $item)
+                    @if(is_array($item))
+                        {{ implode($item) }}
+                    @else
+                        {{ $item }}@if (!$loop->last), @endif
+                    @endif
+                @endforeach
+            </td>
+            @else
             <td>{{ $response->responses[$question['id']] ?? '' }}</td>
+            @endif
         @endforeach
     </tr>
     @endforeach
