d327d229559b74f52c79f5d5e56b4a540bd5dea3
Andy Swick <andreamswick@gmail.com>
2019-02-14 00:05:36 -0600

    Add ticket lookup option and format

--- resources/js/components/genesis/CheckIn.vue
+++ resources/js/components/genesis/CheckIn.vue
@@ -2,19 +2,31 @@
     <div class="flex -mx-4 h-full">
         <router-link to="/" class="absolute pin-t p-2 text-grey-darker no-underline hover:text-grey-darkest">< Back</router-link>
         <div class="shadow-sm bg-white p-8 h-full mx-auto">
-            <h1 class="text-3xl font-normal mb-8 text-blue-darker">Scan Your Ticket</h1>
+            <h1 class="text-2xl font-normal mb-8 text-blue-darker">Scan Your QR Code</h1>
 
             <video id="preview" class="w-64 border border-grey mx-auto mb-4 bg-grey-lighter block mx-auto"></video>
-            <div class="text-center mb-16">
+            <div class="text-center">
                 <button @click="start" class="rounded bg-blue px-4 py-3 text-white font-semibold hover:bg-blue-dark">Start Scan</button>
                 <button @click="stop" class="rounded bg-grey-light px-4 py-3 text-grey-darkest font-semibold hover:bg-grey">Stop</button>
             </div>
 
-            <h2 class="text-2xl font-normal mb-8 text-blue-darker">Or Type Your Confirmation Number</h2>
+            <hr class="my-8 border-b border-grey-light">
+
+            <h1 class="text-2xl font-normal mb-8 text-blue-darker">Type Your Confirmation Number</h1>
             <div class="flex flex-wrap items-stretch w-full mb-4 relative">
                 <input type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border h-10 border-grey-light rounded rounded-r-none px-3 relative" v-model="number">
                 <div class="flex -mr-px">
                     <router-link :to="'/orders/' + number" class="rounded rounded-l-none no-underline bg-blue px-4 flex items-center h-10 text-white font-semibold hover:bg-blue-dark">Continue</router-link>
+                </div>
+            </div>
+
+            <hr class="my-8 border-b border-grey-light">
+
+            <h1 class="text-2xl font-normal mb-8 text-blue-darker">Type Your Ticket Number</h1>
+            <div class="flex flex-wrap items-stretch w-full mb-4 relative">
+                <input type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border h-10 border-grey-light rounded rounded-r-none px-3 relative" v-model="hash">
+                <div class="flex -mr-px">
+                    <router-link :to="'/tickets/' + hash" class="rounded rounded-l-none no-underline bg-blue px-4 flex items-center h-10 text-white font-semibold hover:bg-blue-dark">Continue</router-link>
                 </div>
             </div>
         </div>
@@ -29,6 +41,7 @@
         data() {
             return {
                 number: '',
+                hash: '',
                 error: '',
                 scanner: {},
             }
