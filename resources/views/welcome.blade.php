3b3832f9b96c1a6770d66abdda11c074d283a6b0
Andy Swick <andymswick@gmail.com>
2019-07-30 14:42:09 -0500

    Wrap and flex

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,11 @@
         <div>
             <h1 class="text-center mb-8 text-white text-4xl">Upcoming Events</h1>
 
-            @foreach($upcomingEvents as $upcoming)
-                @include("components.app.event", ['event' => $upcoming])
-            @endforeach
+            <div class="flex flex-wrap">
+                @foreach($upcomingEvents as $upcoming)
+                    @include("components.app.event", ['event' => $upcoming])
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
