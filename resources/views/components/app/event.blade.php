492450eae78517c3ef6bb6ab7a5992cf1ed9f66e
Andy Swick <andymswick@gmail.com>
2019-08-06 18:03:38 -0500

    Add transition to event

--- resources/views/components/app/event.blade.php
+++ resources/views/components/app/event.blade.php
@@ -1,4 +1,4 @@
-<a href="/events/{{ $event->slug }}" class="block w-80 mx-4 bg-gray-100 hover:bg-white rounded-lg overflow-hidden shadow hover:shadow-lg">
+<a href="/events/{{ $event->slug }}" class="transition block w-80 mx-4 bg-gray-100 hover:bg-white rounded-lg overflow-hidden shadow hover:shadow-lg">
     @if($event->image)
         <div class="w-full h-48 bg-cover bg-center" style="background-image: url({{ Storage::url($event->image) }})">
             <img src="{{ Storage::url($event->image) }}" class="hidden" alt="{{ $event->title }} Image">
