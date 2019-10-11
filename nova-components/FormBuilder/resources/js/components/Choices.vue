64ed4b13d3a444c479e0b9ab515bd78b89c8562d
Andy Swick <andymswick@gmail.com>
2019-10-11 18:24:30 -0500

    Fix choices

--- nova-components/FormBuilder/resources/js/components/Choices.vue
+++ nova-components/FormBuilder/resources/js/components/Choices.vue
@@ -1,36 +1,48 @@
 <template>
-    <div class="flex">
-        <div class="w-1/4 px-8 pb-4"><label for="id" class="inline-block text-80 pt-2 leading-tight">Choices</label></div>
-        <div class="w-2/3 px-8 pb-4">
-            <ul>
-                <li v-for="(choice, key) in choices">
-                    <input :id="'id' + key" v-model="choices[key]" placeholder="Choice" type="text"
-                           class="appearance-none bg-transparent border-none w-full text-80 py-1 px-2 leading-tight">
-                </li>
-            </ul>
-            <button @click.prevent="addChoice" class="mt-4 btn btn-default btn-primary">Add Choice</button>
-        </div>
+  <div class="flex">
+    <div class="w-1/4 px-8 pb-4">
+      <label for="id" class="inline-block text-80 pt-2 leading-tight">Choices</label>
     </div>
+    <div class="w-2/3 px-8 pb-4">
+      <ul>
+        <li v-for="(choice, key) in choices">
+          <input
+            :id="'id' + key"
+            v-model="choices[key]"
+            placeholder="Choice"
+            type="text"
+            class="appearance-none bg-transparent border-none w-full text-80 py-1 px-2 leading-tight"
+          />
+        </li>
+      </ul>
+      <button @click.prevent="addChoice" class="mt-4 btn btn-default btn-primary">Add Choice</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['value'],
+export default {
+  props: ["value"],
 
-        data() {
-            return {
-                choices: this.value,
-            }
-        },
-        created() {
-            if(_.isEmpty(this.value)) {
-                this.choices = [];
-            }
-        },
-        methods: {
-            addChoice() {
-                this.choices.push('');
-            }
-        }
+  data() {
+    return {
+      choices: []
+    };
+  },
+  created() {
+    if (!_.isEmpty(this.value)) {
+      this.choices = this.value;
     }
+  },
+  methods: {
+    addChoice() {
+      this.choices.push("");
+    }
+  },
+  watch: {
+    choices() {
+      this.$emit("input", this.choices);
+    }
+  }
+};
 </script>
