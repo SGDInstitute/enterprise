9baa6aa39408818910b4f632053d64a9684d80ac
Andy Swick <andymswick@gmail.com>
2019-12-15 15:30:55 -0600

    Remove button for now *whoops*

--- resources/views/orders/partials/tickets.blade.php
+++ resources/views/orders/partials/tickets.blade.php
@@ -2,11 +2,3 @@
     @includeWhen($ticket->user_id === null, 'orders.partials.emptyTicket')
     @includeWhen($ticket->user_id !== null, 'orders.partials.filledTicket')
 @endforeach
-
-<form action="/orders/{{ $order->id }}/tickets" method="post">
-    @csrf
-    <button class="btn btn-gray">
-        <i class="fa fa-fw fa-plus mr-2"></i>
-        Add Ticket
-    </button>
-</form>
