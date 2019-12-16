c3fb6a68a5e4480a247f9bf1602c80aa63d9f52e
Andy Swick <andymswick@gmail.com>
2019-12-16 15:38:17 -0600

    FIx export

--- app/Nova/Actions/DownloadOrders.php
+++ app/Nova/Actions/DownloadOrders.php
@@ -4,21 +4,22 @@ namespace App\Nova\Actions;
 
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
 use Maatwebsite\LaravelNovaExcel\Actions\DownloadExcel;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class ExportOrders extends DownloadExcel implements WithMapping, WithHeadingRow
+class DownloadOrders extends DownloadExcel implements WithMapping, WithHeadingRow, WithStrictNullComparison
 {
     public function map($order): array
     {
         return [
             $order->id,
-            $order->event->name,
+            $order->event->title,
             $order->user->name,
             $order->user->email,
-            $order->amount,
+            $order->amount / 100,
             $order->tickets->count(),
-            $order->tickets()->completed()->count() / $order->tickets->count() * 100 . '% (' . $order->tickets()->completed()->count() . ')',
+            $order->tickets()->completed()->count(),
             Date::dateTimeToExcel($order->created_at),
         ];
     }
