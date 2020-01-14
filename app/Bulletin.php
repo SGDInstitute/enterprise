582d4f0f68da7bae00d9cfa99290940457fb1e3c
Andy Swick <andymswick@gmail.com>
2020-01-13 18:14:00 -0600

    Add casting

--- app/Bulletin.php
+++ app/Bulletin.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bulletin extends Model
 {
+    protected $casts = [
+        'links' => 'arrray',
+    ];
+
     public function event()
     {
         return $this->belongsTo(Event::class);
