6b35adec2b615963ef715b82202bb44db65b0130
Andy Swick <andymswick@gmail.com>
2020-01-31 12:22:50 -0600

    Make is_queued and is_printed nice and clean booleans

--- app/Queue.php
+++ app/Queue.php
@@ -8,6 +8,10 @@ class Queue extends Model
 {
     protected $fillable = ['batch', 'ticket_id', 'name', 'pronouns', 'tshirt', 'college', 'order_created', 'order_paid', 'completed'];
 
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
+
     public static function complete($ids)
     {
         self::whereIn('id', $ids)->update(['completed' => 1]);
