6b06293f9f095f995a514e1b2eb2e5180db11c57
Andy Swick <andymswick@gmail.com>
2020-02-02 15:29:10 -0600

    Add expo push notifications and checkin notification

---
+++ app/Notifications/CheckInTime.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\Expo\ExpoChannel;
+use NotificationChannels\Expo\ExpoMessage;
+
+class CheckInTime extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [ExpoChannel::class];
+    }
+
+    public function toExpo($notifiable)
+    {
+        return (new ExpoMessage)
+            ->title("It's time to check in!")
+            ->body('Check in now for us to print your name badge')
+            ->setJsonData(['screen' => 'CheckIn']);
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
