2a4d461f8191a7925d14b11e925f4f2c68feef97
Andy Swick <andymswick@gmail.com>
2020-02-05 08:58:48 -0600

    Add json overrides for Forgot password responses

--- app/Http/Controllers/Api/ForgotPasswordController.php
+++ app/Http/Controllers/Api/ForgotPasswordController.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Http\Request;
+
 class ForgotPasswordController extends \App\Http\Controllers\Auth\ForgotPasswordController
 {
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return response()->json(['status', trans($response)]);
+    }
+
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return response()->json(['email' => $request->only('email'), 'errors' => ['email' => trans($response)]]);
+    }
 }
