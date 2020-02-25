7b746dc23d79bcca1611aa62489175945a8c90a0
Andy Swick <andymswick@gmail.com>
2020-02-24 18:15:19 -0600

    Remove checkin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,15 @@
 let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
-mix.webpackConfig({
-    node: {
-        fs: "empty"
-    },
-});
-
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/checkin.js', 'public/js')
-    .less('resources/less/app.less', 'public/css')
-    .less('resources/less/checkin.less', 'public/css')
-    .options({
-        processCssUrls: false,
-        postCss: [
-            tailwindcss('./tailwind.config.js')
-        ],
-    });
+   .less('resources/less/app.less', 'public/css')
+   .options({
+      processCssUrls: false,
+      postCss: [
+         tailwindcss('./tailwind.config.js')
+      ],
+   });
 
 if (mix.inProduction()) {
-    mix.version();
+   mix.version();
 }
