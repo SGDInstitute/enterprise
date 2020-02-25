b2fd07f0ac19ce7e35c8b6efd1e44d56ca116dea
Andy Swick <andymswick@gmail.com>
2020-02-24 18:14:32 -0600

    Update MBLGTACC for 2021

--- database/seeds/EventsSeeder.php
+++ database/seeds/EventsSeeder.php
@@ -19,15 +19,15 @@ class EventsSeeder extends Seeder
     private function createMBLGTACC()
     {
         $event = Event::create([
-            'title' => 'MBLGTACC 2020',
-            'subtitle' => 'All Roads Lead to Intersectionality',
-            'timezone' => 'America/Detroit',
-            'place' => 'Western Michigan University,',
-            'location' => 'Kalamazoo, Michigan',
-            'slug' => 'mblgtacc-2020',
+            'title' => 'MBLGTACC 2021',
+            'subtitle' => 'From Protest and Beyond Pride',
+            'timezone' => 'America/Chicago',
+            'place' => 'University of Wisconsin Maddison',
+            'location' => 'Maddison, Wisconsin',
+            'slug' => 'mblgtacc-2021',
             'stripe' => 'mblgtacc',
-            'start' => '2020-02-16 19:00:00',
-            'end' => '2020-02-18 19:30:00',
+            'start' => '2021-02-12 19:00:00',
+            'end' => '2021-02-14 19:30:00',
             'ticket_string' => 'attendee',
             'description' => 'The Midwest Bisexual Lesbian Gay Transgender Asexual College Conference (MBLGTACC) is an annual conference held to connect, educate, and empower LGBTQIA+ college students, faculty, and staff around the Midwest and beyond. It has attracted advocates and thought leaders including Angela Davis, Robyn Ochs, Janet Mock, Laverne Cox, Kate Bornstein, Faisal Alam, and LZ Granderson; and entertainers and artists including RuPaul, Margaret Cho, J Mase III, Chely Wright, and Loren Cameron.',
             'published_at' => \Carbon\Carbon::now()->subMonth(),
@@ -38,23 +38,23 @@ class EventsSeeder extends Seeder
                 ['icon' => 'snapchat-ghost', 'link' => 'https://www.snapchat.com/add/mblgtacc', 'order' => 4],
                 ['icon' => 'website', 'link' => 'https://mblgtacc.org', 'order' => 5],
             ],
-            'image' => 'https://mblgtacc.org/themes/mblgtacc2018/assets/images/arts-and-sciences-fall.jpg',
-            'logo_light' => 'https://mblgtacc.org/themes/mblgtacc2018/assets/images/mblgtacc-2018-horiz_White.png',
-            'logo_dark' => 'https://mblgtacc.org/themes/mblgtacc2018/assets/images/mblgtacc-2018-horiz_Gray.png',
-            'refund_policy' => '<p>The regular registration rate is available until January 17, 2018. Full refunds are available for cancelled orders until this date. Cancellations received after this date will not be eligible for a refund. Refunds are not available for attendees who choose not to attend the event if the cancellation is not received before the stated cancellation deadline. Orders may be transferred to another attendee at no charge, at any time. Cancellations or transfers must be requested by the user who created the order and should include the name of the attendee. To request a cancellation and refund, or to transfer your ticket to another attendee, please email <a href="mailto:kate@sgdinstitute.org">kate@sgdinstitute.org</a>.</p>',
+            'image' => 'https://mblgtacc.org/assets/1855_colton_map_of_the_united_states_-_geographicus_-_unitedstates-colton-1855-publicdomain-cropcolor.jpg',
+            'logo_light' => 'https://mblgtacc.org/assets/2021/mblgtacc-2021-horiz_white.png',
+            'logo_dark' => 'https://mblgtacc.org/assets/2021/mblgtacc-2021-horiz_gray.png',
+            'refund_policy' => '<p>The regular registration rate is available until January 17, 2020. Full refunds are available for cancelled orders until this date. Cancellations received after this date will not be eligible for a refund. Refunds are not available for attendees who choose not to attend the event if the cancellation is not received before the stated cancellation deadline. Orders may be transferred to another attendee at no charge, at any time. Cancellations or transfers must be requested by the user who created the order and should include the name of the attendee. To request a cancellation and refund, or to transfer your ticket to another attendee, please email <a href="mailto:support@sgdinstitute.org">support@sgdinstitute.org</a>.</p>',
         ]);
 
         $event->ticket_types()->createMany([
             [
                 'name' => 'Regular Registration',
-                'cost' => 7500,
+                'cost' => 8500,
                 'availability_start' => $event->start->subMonth(6),
                 'availability_end' => $event->start->subMonth(),
             ],
             [
                 'name' => 'Late Registration',
                 'description' => 'You are not guarenteed special items such as T-Shirts, Program Books, etc. Extras will be available on Sunday after the closing ceremony.',
-                'cost' => 7500,
+                'cost' => 8500,
                 'availability_start' => $event->start->subMonth(),
                 'availability_end' => $event->end,
             ],
