32c26b7fffcb18099f9e6aeb2d7f12fc13f1903c
Andy M. Newhouse <hi@andymnewhouse.me>
2020-09-12 23:41:14 -0500

    Republish mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,17 +1,11 @@
 <tr>
 <td class="header">
-<table>
-<tr>
-<td class="column-1">
-<a href="https://sgdinstitute.org" target="_blank" class="institute-logo">
-<img alt="Midwest Institute for Sexuality and Gender Diversity"
-src="https://sgdinstitute.org/storage/app/media/emails/logo-dark-text.png"/>
-</a>
-</td>
-<td class="column-2">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
 {{ $slot }}
-</td>
-</tr>
-</table>
+@endif
+</a>
 </td>
 </tr>
