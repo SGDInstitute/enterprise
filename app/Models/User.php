f3a5c69aa6898a1ccd5bf6dec7990a99b9ef47e0
Andy Newhouse <hi@andymnewhouse.me>
2021-07-15 14:11:50 -0500

    Add impersonating users and fix stephanie’s issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,21 +7,15 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
+use Lab404\Impersonate\Models\Impersonate;
 use Laravel\Cashier\Billable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, HasRoles, Billable, HasProfilePhoto;
+    use HasFactory, Notifiable, HasRoles, Billable, HasProfilePhoto, Impersonate;
 
     protected $guarded = [];
-    // protected $fillable = [
-    //     'name',
-    //     'email',
-    //     'password',
-    //     'pronouns',
-    // ];
 
     protected $hidden = [
         'password',
