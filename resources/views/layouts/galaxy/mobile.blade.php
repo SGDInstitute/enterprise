a75882840d7d5ea9a3c50ff15f3c7570a4a10f1e
Andy Newhouse <hi@andymnewhouse.me>
2021-10-19 15:43:14 -0500

    Mobile nav dark mode

--- resources/views/layouts/galaxy/mobile.blade.php
+++ resources/views/layouts/galaxy/mobile.blade.php
@@ -3,17 +3,16 @@
         <div @click="sidebarOpen = false" x-show="sidebarOpen" x-description="Off-canvas menu overlay, show/hide based on off-canvas menu state." x-transition:enter="transition-opacity ease-linear duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition-opacity ease-linear duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0" aria-hidden="true" style="display: none;">
             <div class="absolute inset-0 bg-gray-600 opacity-75"></div>
         </div>
-        <div x-show="sidebarOpen" x-description="Off-canvas menu, show/hide based on off-canvas menu state." x-transition:enter="transition ease-in-out duration-300 transform" x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full" class="relative flex flex-col flex-1 w-full max-w-xs pt-5 pb-4 bg-white" style="display: none;">
+        <div x-show="sidebarOpen" x-description="Off-canvas menu, show/hide based on off-canvas menu state." x-transition:enter="transition ease-in-out duration-300 transform" x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full" class="relative flex flex-col flex-1 w-full max-w-xs pt-5 pb-4 bg-white dark:bg-gray-900" style="display: none;">
             <div class="absolute top-0 right-0 pt-2 -mr-12">
                 <button x-show="sidebarOpen" @click="sidebarOpen = false" class="flex items-center justify-center w-10 h-10 ml-1 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" style="display: none;">
                     <span class="sr-only">Close sidebar</span>
-                    <svg class="w-6 h-6 text-white" x-description="Heroicon name: x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                    </svg>
+                    <x-heroicon-o-x class="w-6 h-6 text-white" x-description="Heroicon name: x"/>
                 </button>
             </div>
             <div class="flex items-center flex-shrink-0 px-4">
-                <img class="w-auto h-8" src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow">
+                <img class="w-auto h-8" src="/img/galaxy.png" alt="Galaxy">
+                <span class="ml-4 text-xl dark:text-white">Galaxy</span>
             </div>
             <div class="flex-1 h-0 mt-5 overflow-y-auto">
                 <nav class="px-2 space-y-1">
