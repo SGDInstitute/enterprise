a68e5924f416dd470a0f4494eb9a6e0d1663fd11
Andy Newhouse <hi@andymnewhouse.me>
2022-03-21 20:03:40 -0500

    Add help text

--- resources/views/components/bit/input/help.blade.php
+++ resources/views/components/bit/input/help.blade.php
@@ -1,3 +1,3 @@
-<p {{ $attributes->merge(['class' => 'mt-2 text-sm text-gray-500 dark:text-gray-400']) }}>
-    {{ $slot }}
-</p>
+<div {{ $attributes->merge(['class' => 'mt-2 text-sm text-gray-500 dark:text-gray-400']) }}>
+    {!! markdown($slot) !!}
+</div>
--- resources/views/livewire/app/forms/partials/collaborators.blade.php
+++ resources/views/livewire/app/forms/partials/collaborators.blade.php
@@ -1,7 +1,7 @@
 <div>
     <p><strong>Collaborators:</strong></p>
     <x-bit.input.group :for="$item['id']" label="Add the emails of those who will be presenting with you (if any)">
-        <x-bit.input.textarea class="w-full mt-1" :id="$item['id']" wire:model="answers.{{ $item['id'] }}" :disabled="!$fillable" />
+        <x-bit.input.textarea class="w-full mt-1" :id="$item['id']" wire:model.lazy="answers.{{ $item['id'] }}" :disabled="!$fillable" />
         <x-bit.input.help>Put each email on a new line or separate with commas.</x-bit.input.help>
         <x-bit.input.help>They will be added as a collaborator and will be allowed to make changes to this submission.</x-bit.input.help>
     </x-bit.input.group>
