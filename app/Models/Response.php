7ce58a8c2e171e3819423c89c9ae508209487dbd
Andy Newhouse <hi@andymnewhouse.me>
2022-03-21 20:01:02 -0500

    Update method for new version of Activitylogger

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -5,15 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Response extends Model
 {
     use HasFactory;
     use LogsActivity;
 
-    protected static $logAttributes = ['answers', 'status'];
-
-    protected static $logOnlyDirty = true;
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['answers', 'status'])
+            ->logOnlyDirty();
+    }
 
     public $guarded = [];
 
