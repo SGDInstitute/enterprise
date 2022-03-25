c7943daded9f0fa38b7f5dca2ee874c16104889a
Andy Newhouse <hi@andymnewhouse.me>
2022-03-25 17:58:39 -0500

    Add dark background/border to radio and checkbox

--- resources/views/components/bit/input/checkbox.blade.php
+++ resources/views/components/bit/input/checkbox.blade.php
@@ -6,7 +6,7 @@
 @if ($label)
 <div class="relative flex items-start">
     <div class="flex items-center h-5">
-        <input {{ $attributes }} type="checkbox" class="w-4 h-4 text-green-600 border-gray-300 rounded focus:ring-green-500 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}">
+        <input {{ $attributes }} type="checkbox" class="w-4 h-4 text-green-600 dark:bg-gray-800 border-gray-300 dark:border-gray-500 rounded focus:ring-green-500 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}">
     </div>
     <div class="ml-3 text-sm">
         <label for="{{ $attributes->get('id') }}" class="font-medium text-gray-700 dark:text-gray-200 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}">{{ $label }}</label>
@@ -19,7 +19,7 @@
 <div class="flex">
     <input {{ $attributes }}
         type="checkbox"
-        class="block transition duration-150 ease-in-out border-gray-300 rounded form-checkbox sm:text-sm sm:leading-5 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}"
+        class="block transition duration-150 ease-in-out border-gray-300 dark:bg-gray-800 dark:border-gray-500 rounded form-checkbox sm:text-sm sm:leading-5 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}"
     />
 </div>
 @endif
