3a07d19afa6fa85cb0808063bf91da4dde78325a
Andy Newhouse <hi@andymnewhouse.me>
2022-04-05 16:25:22 -0500

    Move collaborators to table and disable buttons if not fillable
    Fixes #78

--- resources/views/livewire/app/forms/partials/collaborators.blade.php
+++ resources/views/livewire/app/forms/partials/collaborators.blade.php
@@ -1,28 +1,31 @@
 <div class="space-y-2">
     <label for="question-collaborators" class="block font-medium leading-5 text-gray-700 dark:text-gray-200">Presenters</label>
 
-    <div>
-        <div class="grid grid-cols-7 gap-2 mb-2">
-            <span class="block col-span-2 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">Email</span>
-            <span class="block col-span-2 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">Name</span>
-            <span class="block col-span-2 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">Pronouns</span>
-            <span class="block text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300"></span>
-        </div>
-        <div class="space-y-1">
+    <table>
+        <thead>
+            <tr>
+                <th class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">Email</th>
+                <th class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">Name</th>
+                <th class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">Pronouns</th>
+                <th class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase dark:text-gray-300"></th>
+            </tr>
+        </thead>
+        <tbody>
             @foreach ($collaborators as $index => $collaborator)
-            <div wire:key="row-{{ $collaborator['id'] ?? $index }}" class="grid grid-cols-7 gap-2">
-                <p class="col-span-2 m-0">{{ $collaborator['email'] }}</p>
-                <p class="col-span-2 m-0">{{ $collaborator['name'] }}</p>
-                <p class="col-span-2 m-0">{{ $collaborator['pronouns'] }}</p>
-
-                <x-bit.button.round.secondary block wire:click="deleteCollaborator({{ $collaborator['id'] }})" :disabled="$response['user_id'] === $collaborator['id'] ?? $index === 0">
-                    <x-heroicon-o-trash class="w-4 h-4" />
-                    <span class="sr-only">Remove Presenter</span>
-                </x-bit.button.round.secondary>
-            </div>
+            <tr wire:key="row-{{ $collaborator['id'] ?? $index }}">
+                <td>{{ $collaborator['email'] }}</td>
+                <td>{{ $collaborator['name'] }}</td>
+                <td>{{ $collaborator['pronouns'] }}</td>
+                <td>
+                    <x-bit.button.round.secondary block wire:click="deleteCollaborator({{ $collaborator['id'] }})" :disabled="!$fillable || $response['user_id'] === $collaborator['id']">
+                        <x-heroicon-o-trash class="w-4 h-4" />
+                        <span class="sr-only">Remove Presenter</span>
+                    </x-bit.button.round.secondary>
+                </td>
+            </tr>
             @endforeach
-        </div>
-    </div>
+        </tbody>
+    </table>
 
-    <x-bit.button.round.secondary wire:click="$set('showCollaboratorModal', true)">Add presenter</x-bit.button.round.secondary>
+    <x-bit.button.round.secondary wire:click="$set('showCollaboratorModal', true)" :disabled="!$fillable">Add presenter</x-bit.button.round.secondary>
 </div>
