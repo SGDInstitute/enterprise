d3ec14fad85afbb5d3125a0de89c199e8ef89410
Andy Newhouse <hi@andymnewhouse.me>
2022-05-18 19:08:40 -0500

    Add Tailwind UI components

---
+++ resources/views/components/ui/card/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="flex justify-between px-4 py-5 space-x-4 bg-white border-t border-gray-200 dark:bg-gray-800 dark:border-gray-700 sm:px-6">
+    {{ $slot }}
+</div>
---
+++ resources/views/components/ui/card/index.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'overflow-hidden bg-white dark:bg-gray-800 dark:border dark:border-gray-700 sm:rounded-lg sm:shadow']) }}>
+    {{ $slot }}
+</div>
