67ca6d63393f6d25b1c76e09aa19c9192862e9b4
Andy Newhouse <hi@andymnewhouse.me>
2022-09-28 17:09:14 -0500

    Remove sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -35,9 +35,7 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            if (app()->bound('sentry')) {
-                app('sentry')->captureException($e);
-            }
+            //
         });
     }
 }
