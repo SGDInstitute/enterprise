ce2843906c9746b1ed8ede1fccae54948356c910
Andy Newhouse <hi@andymnewhouse.me>
2022-10-18 15:12:51 -0500

    Make name badge component

--- resources/views/livewire/app/checkin/in-queue.blade.php
+++ resources/views/livewire/app/checkin/in-queue.blade.php
@@ -1,13 +1,5 @@
 <div class="mt-8 space-y-4">
-    <div class="relative">
-        <div class="absolute flex items-center justify-center w-full h-full">
-            <div>
-                <h1 id="name" class="mt-8 mb-2 text-4xl font-semibold leading-none tracking-wide text-center font-raleway">{{ $user->name }}</h1>
-                <p id="pronouns" class="mb-1 font-sans text-2xl leading-none text-center">{{ $user->pronouns }}</p>
-            </div>
-        </div>
-        <img src="{{ asset('img/mblgtacc30-name-badge-blank.jpg') }}" class="w-full" alt="Name Badge Background">
-
+    <x-name-badge :user="$user">
         <div class="absolute flex items-center px-4 py-1 ml-2 space-x-2 text-blue-900 bg-green-500 rounded-l-full shadow -right-1 bottom-4">
             @if ($ticket->isPrinted())
                 <p class="font-bold text-center text-gray-700 md:text-xl dark:text-gray-200">Your badge is ready!!</p>
@@ -15,7 +7,7 @@
                 <p class="font-bold text-center text-gray-700 md:text-xl dark:text-gray-200">You are checked in and we are preparing your badge.</p>
             @endif
         </div>
-    </div>
+    </x-name-badge>
 
     <x-bit.alert wire:poll.750ms>We've got {{ $position }} badges to print before yours, please don't come to the desk until you get a notification that your badge is ready.</x-bit.alert>
 
--- resources/views/livewire/app/program/badge.blade.php
+++ resources/views/livewire/app/program/badge.blade.php
@@ -3,15 +3,5 @@
         <h1>Badge</h1>
     </div>
 
-    <div class="relative w-full h-64 bg-white shadow-md dark:bg-gray-200 lg:h-96">
-        <img src="{{ asset('img/name-badge-background-top.png') }}" class="absolute w-full" alt="Name Badge Background">
-        <div class="flex items-center justify-center w-full h-full">
-            <div>
-                <h1 id="name" class="mt-8 mb-2 text-3xl font-semibold leading-none tracking-wide text-center font-raleway">{{ $user->name }}</h1>
-                @if ($user->pronouns)
-                <p id="pronouns" class="mb-1 font-sans text-xl leading-none text-center">{{ $user->pronouns }}</p>
-                @endif
-            </div>
-        </div>
-    </div>
+    <x-name-badge :user="$user" />
 </div>
