f324d9431e8376de83c9eb79c2e4d83ba18a0b1d
Andy Newhouse <hi@andymnewhouse.me>
2022-10-18 15:33:21 -0500

    Improve checkbox

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -3,10 +3,10 @@
 @isset ($label)
 <div class="relative flex items-start">
     <div class="flex items-center h-6">
-        <input id="{{ $id }}" name="{{ $label }}" {{ $attributes }} type="checkbox" class="w-5 h-5 text-green-600 border-gray-300 rounded dark:border-gray-700 dark:bg-gray-800 focus:ring-green-500">
+        <input id="{{ $id }}" name="{{ $id }}" {{ $attributes }} type="checkbox" class="w-5 h-5 text-green-600 border-gray-300 rounded dark:border-gray-700 dark:bg-gray-800 focus:ring-green-500">
     </div>
     <div class="ml-2">
-        <label for="{{ $label }}" class="font-medium text-gray-700 dark:text-gray-200">{{ $label }}</label>
+        <label for="{{ $id }}" class="font-medium text-gray-700 dark:text-gray-200">{{ $label }}</label>
     </div>
 </div>
 @else
