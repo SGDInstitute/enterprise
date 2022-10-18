e3fa6d14be7d54f41de1d23e62d4dbe1edb6dc56
Andy Newhouse <hi@andymnewhouse.me>
2022-10-18 15:52:48 -0500

    Add notify to creating bulletins

--- app/Http/Livewire/Galaxy/Events/Edit/Bulletin.php
+++ app/Http/Livewire/Galaxy/Events/Edit/Bulletin.php
@@ -24,6 +24,7 @@ class Bulletin extends Component
         'bulletin.content' => 'required',
         'formattedPublish' => 'required',
         'bulletin.timezone' => 'required',
+        'bulletin.notify' => 'boolean',
     ];
 
     public function mount()
