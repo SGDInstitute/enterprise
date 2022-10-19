983fa0bd1f249aa8a211cddc5332bccceb5ca109
Andy Newhouse <hi@andymnewhouse.me>
2022-10-18 22:13:29 -0500

    Update language and styling

--- resources/views/livewire/app/checkin/in-queue.blade.php
+++ resources/views/livewire/app/checkin/in-queue.blade.php
@@ -1,15 +1,13 @@
 <div class="mt-8 space-y-4">
     <x-name-badge :user="$user">
-        <div class="absolute flex items-center px-4 py-1 ml-2 space-x-2 text-blue-900 bg-green-500 rounded-l-full shadow -right-1 bottom-4">
+        <div class="absolute flex items-center px-4 py-2 ml-4 bg-green-500 shadow-[0_6px_2px_-2px_rgba(0,0,0,0.3)] md:ml-8 -right-1 bottom-4 text-gray-200">
             @if ($ticket->isPrinted())
-                <p class="font-bold text-center text-gray-700 md:text-xl dark:text-gray-200">Your badge is ready!!</p>
+                <p>Your badge is ready!!</p>
             @else
-                <p class="font-bold text-center text-gray-700 md:text-xl dark:text-gray-200">You are checked in and we are preparing your badge.</p>
+                <p wire:poll.750ms>There are {{ $position }} badges printing ahead of yours. Please wait for the notification that your badge is ready, and then come to the check-in desk to pick it up.</p>
             @endif
         </div>
     </x-name-badge>
-
-    <x-bit.alert wire:poll.750ms>We've got {{ $position }} badges to print before yours, please don't come to the desk until you get a notification that your badge is ready.</x-bit.alert>
 
     <div class="flex justify-center">
         <x-bit.button.flat.secondary size="lg" :href="route('app.program', $ticket->event)">View Virtual Program Book</x-bit.button.flat.secondary>
