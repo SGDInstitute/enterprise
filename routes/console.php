b1289157aa54f887be0b3f75e0d50ea1875f1642
Andy Newhouse <hi@andymnewhouse.me>
2022-10-21 00:16:53 -0400

    Get real data from the API, add Test print

--- routes/console.php
+++ routes/console.php
@@ -1,27 +1,51 @@
 <?php
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
-use Wnx\SidecarBrowsershot\BrowsershotLambda;
+use Spatie\Browsershot\Browsershot;
 
-Artisan::command('print', function () {
-    $attendees = [
-        ['name' => 'Andy Newhouse', 'pronouns' => 'they/them'],
-        ['name' => 'Jess Newhouse', 'pronouns' => 'they/them'],
-        ['name' => 'Justin Drwencke', 'pronouns' => 'they/them'],
-        ['name' => 'R.B. Brooks', 'pronouns' => 'they/them'],
-        ['name' => 'Michelle Walters', 'pronouns' => 'they/them'],
+Artisan::command('print:all', function () {
+    $url = env('PROD_URL');
+
+    while (true) {
+        $this->info("Getting attendees");
+        $attendees = Http::get($url . '/api/queue')->json();
+
+        foreach ($attendees as $attendee) {
+            $slug = Str::slug($attendee['name']);
+            $view = "<h1 style='text-align:center;font-size:72px'>{$attendee['name']}</h1><h2 style='text-align:center;font-size:48px'>{$attendee['pronouns']}</h2>";
+            $path = storage_path("app/labels/{$slug}.png");
+
+            Browsershot::html($view)
+                ->windowSize(991, 306)
+                ->save($path);
+
+                $this->info("Printing: {$attendee['name']} : {$attendee['pronouns']}");
+            exec("brother_ql -b pyusb print -l 29x90 {$path}");
+
+            Http::post($url . "/api/queue/{$attendee['ticket_id']}/printed");
+        }
+
+        $this->info("Sleeping for 30 seconds");
+        sleep(30);
+    }
+});
+
+Artisan::command('print:test', function () {
+    $attendee = [
+        'name' => fake()->name,
+        'pronouns' => fake()->randomElement(['she/her', 'he/him', 'they/them', 'zie/zir', 'Any Pronouns']),
     ];
 
-    foreach ($attendees as $attendee) {
-        $slug = Str::slug($attendee['name']);
-        $view = "<h1 style='text-align:center;font-size:72px'>{$attendee['name']}</h1><h2 style='text-align:center;font-size:48px'>{$attendee['pronouns']}</h2>";
-        $path = storage_path("labels/{$slug}.png");
+    $slug = Str::slug($attendee['name']);
+    $view = "<h1 style='text-align:center;font-size:72px'>{$attendee['name']}</h1><h2 style='text-align:center;font-size:48px'>{$attendee['pronouns']}</h2>";
+    $path = storage_path("app/labels/{$slug}.png");
 
-        BrowsershotLambda::html($view)
-            ->windowSize(991, 306)
-            ->save($path);
+    Browsershot::html($view)
+        ->windowSize(991, 306)
+        ->save($path);
 
-        exec("brother_ql -b pyusb print -l 29x90 {$path}");
-    }
-})->purpose('Display an inspiring quote');
+    $this->info("Printing: {$attendee['name']} : {$attendee['pronouns']}");
+    exec("brother_ql -b pyusb print -l 29x90 {$path}");
+});
