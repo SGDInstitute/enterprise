f7f3e3430cddfd148f7dbd31fdc205980a575a76
Andy Newhouse <hi@andymnewhouse.me>
2023-03-15 13:21:33 -0500

    Start working on stats overview card

--- app/Filament/Resources/EventResource/Pages/ViewEvent.php
+++ app/Filament/Resources/EventResource/Pages/ViewEvent.php
@@ -4,8 +4,10 @@ namespace App\Filament\Resources\EventResource\Pages;
 
 use App\Filament\Resources\EventResource;
 use App\Filament\Resources\EventResource\Widgets\DaysUntilEvent;
+use App\Filament\Resources\EventResource\Widgets\StatsOverview;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class ViewEvent extends ViewRecord
 {
@@ -23,7 +25,7 @@ class ViewEvent extends ViewRecord
     protected function getHeaderWidgets(): array
     {
         return [
-            //
+            StatsOverview::class,
         ];
     }
 }
