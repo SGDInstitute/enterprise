aeab68b609488ff8be274ede9465a4c7de109f5f
Andy Newhouse <hi@andymnewhouse.me>
2023-03-14 20:31:26 -0500

    Add custom view page

--- app/Filament/Resources/EventResource/Pages/ViewEvent.php
+++ app/Filament/Resources/EventResource/Pages/ViewEvent.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\EventResource\Pages;
 
 use App\Filament\Resources\EventResource;
+use App\Filament\Resources\EventResource\Widgets\DaysUntilEvent;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -10,10 +11,19 @@ class ViewEvent extends ViewRecord
 {
     protected static string $resource = EventResource::class;
 
+    protected static string $view = 'filament.resources.events.pages.view-event';
+
     protected function getActions(): array
     {
         return [
             Actions\EditAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            //
         ];
     }
 }
