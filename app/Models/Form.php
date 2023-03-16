1a049b9af28d5e30465e47cf261cd8c5ada8a7b6
Andy Newhouse <hi@andymnewhouse.me>
2023-03-16 18:18:21 -0500

    Add timezone property

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -88,6 +88,16 @@ class Form extends Model
         return $this->start->timezone($this->timezone)->format('m/d/Y g:i A');
     }
 
+    public function getFormattedTimezoneAttribute()
+    {
+        if ($this->timezone === 'America/New_York') {
+            return 'EST';
+        }
+        if ($this->timezone === 'America/Chicago') {
+            return 'CST';
+        }
+    }
+
     public function getHasCollaboratorsAttribute()
     {
         return $this->form->contains('style', 'collaborators');
