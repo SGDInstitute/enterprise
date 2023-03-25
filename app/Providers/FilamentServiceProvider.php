0730ae71d478fac46bf67e07c7a4483802781bac
Andy Newhouse <hi@andymnewhouse.me>
2023-03-25 15:08:40 -0500

    Customize filament theme

---
+++ app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Providers;
+
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Filament::serving(function () {
+            Filament::registerViteTheme('resources/css/filament.css');
+        });
+
+        $this->registerSkipToContentLink();
+    }
+
+    private function registerSkipToContentLink()
+    {
+        Filament::registerRenderHook(
+            'styles.end',
+            fn(): string => Blade::render(<<<HTML
+                <style>
+                    #skip-to-content a {
+                        padding: 10px 15px 15px 10px;
+                        position: absolute;
+                        top: -100px;
+                        left: 0;
+                        color: white;
+                        border-right: 1px solid #FFFFFF;
+                        border-bottom: 1px solid #FFFFFF;
+                        -webkit-transition: top 1s ease-out;
+                        transition: top 1s ease-out;
+                        z-index: 10000;
+                    }
+
+                    #skip-to-content a:focus {
+                        position: absolute;
+                        left: 0;
+                        top: 0;
+                        outline-color: transparent;
+                        -webkit-transition: top 0.1s ease-in;
+                        transition: top 0.1s ease-in;
+                    }
+                </style>
+            HTML)
+        );
+
+        Filament::registerRenderHook(
+            'body.start',
+            fn(): string => Blade::render(<<<HTML
+                <div id="skip-to-content">
+                    <a class="bg-primary-500" href="#main-content">Skip to main content</a>
+                </div>
+            HTML)
+        );
+
+        Filament::registerRenderHook(
+            'content.start',
+            fn(): string => Blade::render('<div id="main-content"></div>')
+        );
+    }
+}
