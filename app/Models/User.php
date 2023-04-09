393b87b17cb375d3631b2ae6c2d9f2e659fb2ca0
Andy Newhouse <hi@andymnewhouse.me>
2023-04-09 12:25:19 -0500

    Use roles > permissions for now

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return $this->can('view-galaxy');
+        return $this->hasAnyRole(['institute', 'mblgtacc_planner', 'developer']);
     }
 
     // Relationships
