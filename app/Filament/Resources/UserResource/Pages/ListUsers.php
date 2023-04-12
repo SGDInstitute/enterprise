57a52edae5923a916b0bfa8bd4ccec5bcc70d739
Andy Newhouse <hi@andymnewhouse.me>
2023-04-11 19:12:41 -0500

    Add action

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Pages\Actions;
+use Filament\Pages\Actions\Action;
+use Filament\Pages\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
@@ -13,7 +16,18 @@ class ListUsers extends ListRecords
     protected function getActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make(),
+            Action::make('mass-delete-junk')
+                ->action(function () {
+                    User::query()
+                        ->whereNull('email_verified_at')
+                        ->whereDoesntHave('donations')
+                        ->whereDoesntHave('orders')
+                        ->whereDoesntHave('responses')
+                        ->whereDoesntHave('schedule')
+                        ->whereDoesntHave('tickets')
+                        ->delete();
+                }),
         ];
     }
 }
