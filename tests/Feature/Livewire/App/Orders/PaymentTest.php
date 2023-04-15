76c9a7fd3de4168a21c4bb47b3b6283420d183a1
Andy Newhouse <hi@andymnewhouse.me>
2023-04-15 11:44:36 -0500

    Add test case

---
+++ tests/Feature/Livewire/App/Orders/PaymentTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature\Livewire\App\Orders;
+
+use App\Http\Livewire\App\Orders\Payment;
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class PaymentTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function the_component_can_render()
+    {
+        $user = User::factory()->create();
+        $order = Order::factory()->for($user)->hasTickets(2)->create();
+        
+        Livewire::actingAs($user)
+            ->test(Payment::class, ['order' => $order])
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function can_download_invoice()
+    {
+        $user = User::factory()->create();
+        $order = Order::factory()->for($user)->hasTickets(2)->create();
+        
+        Livewire::actingAs($user)
+            ->test(Payment::class, ['order' => $order])
+            ->call('downloadInvoice')
+            ->assertHasErrors([
+                'address.line1' => 'required',
+                'address.city' => 'required',
+                'address.state' => 'required',
+                'address.zip' => 'required',
+                'address.country' => 'required',
+            ])
+            ->set('address.line1', '123 Main')
+            ->set('address.city', 'Gravesfield')
+            ->set('address.state', 'CT')
+            ->set('address.zip', '06109')
+            ->set('address.country', 'United States')
+            ->call('downloadInvoice')
+            ->assertFileDownloaded();
+    }
+}
