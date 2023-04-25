886f58a7b3b31a3baa4001cedfd52db118bce3ec
Andy Newhouse <hi@andymnewhouse.me>
2023-04-25 12:19:18 -0500

    Add guided registration modal

--- app/Http/Livewire/App/Events/Show.php
+++ app/Http/Livewire/App/Events/Show.php
@@ -3,24 +3,35 @@
 namespace App\Http\Livewire\App\Events;
 
 use App\Models\Event;
+use App\Models\Order;
+use App\Models\Ticket;
 use Livewire\Component;
 
 class Show extends Component
 {
     public Event $event;
 
+    public $guide =  [
+        'num_tickets' => 1,
+        'is_attending' => null,
+        'payment' => null,
+    ];
+
     public $showPreviousOrders = false;
+    public $showGuide = false;
 
     protected $listeners = ['refresh' => '$refresh'];
 
     public function mount()
     {
         if (request()->query('edit')) {
-            // check if user is authorized to view
+            // @todo check if user is authorized to view
             $this->load(request()->query('edit'));
         } else {
             if ($this->previousOrders->count() > 0) {
                 $this->showPreviousOrders = true;
+            } else {
+                $this->showGuide = true;
             }
         }
     }
@@ -49,6 +60,53 @@ class Show extends Component
             ['title' => 'Pay Now or Get Invoice', 'complete' => false, 'current' => false],
             ['title' => 'Assign Tickets', 'complete' => false, 'current' => false],
         ];
+    }
+
+    public function generate()
+    {
+        $this->validate([
+            'guide.num_tickets' => ['required', 'integer', 'min:1'],
+            'guide.is_attending' => ['required', 'boolean'],
+            'guide.payment' => ['required', 'boolean'],
+        ], attributes: [
+            'guide.num_tickets' => 'number of tickets',
+            'guide.is_attending' => '"are you attending"',
+            'guide.payment' => 'payment method',
+        ]);
+
+        $reservation = Order::create(['event_id' => $this->event->id, 'user_id' => auth()->id(), 'reservation_ends' => now()->addDays($this->event->reservationEndsAt)]);
+        $reservation->tickets()->createMany($this->convertFormToTickets());
+
+        return redirect()->route('app.orders.show', $reservation);
+    }
+
+    public function showGuideModal()
+    {
+        $this->showPreviousOrders = false;
+        $this->showGuide = true;
+    }
+
+    private function convertFormToTickets()
+    {
+        // @todo handle if more than one type of ticket option
+        $ticketType = $this->event->ticketTypes()->where('end', '>', now())->get()->first();
+
+        $data = [
+            'event_id' => $this->event->id,
+            'ticket_type_id' => $ticketType->id,
+            'price_id' => $ticketType->prices->first()->id,
+        ];
+
+        $tickets = [];
+        foreach (range(1, $this->guide['num_tickets']) as $count) {
+            if ($this->guide['is_attending'] == true && $count === 1) {
+                $tickets[] = array_merge(['user_id' => auth()->id()], $data);
+            } else {
+                $tickets[] = $data;
+            }
+        }
+
+        return $tickets;
     }
 
     private function load($id)
--- tests/Feature/Livewire/App/Events/ShowTest.php
+++ tests/Feature/Livewire/App/Events/ShowTest.php
@@ -32,4 +32,30 @@ class ShowTest extends TestCase
             ->test(Show::class, ['event' => $event])
             ->assertSee('verify your email');
     }
+
+    /** @test */
+    public function can_generate_order_from_guide()
+    {
+        $user = User::factory()->create();
+        $event = Event::factory()->preset('mblgtacc')->create();
+        TicketType::factory()->for($event)->count(2)
+            ->state(new Sequence(
+                ['name' => 'Expired', 'end' => now()->subDays(7)],
+                ['name' => 'Available', 'end' => now()->addDays(7)],
+            ))
+            ->hasPrices(1)
+            ->create();
+
+        Livewire::actingAs($user)
+            ->test(Show::class, ['event' => $event])
+            ->set('guide.num_tickets', 2)
+            ->set('guide.is_attending', '1')
+            ->set('guide.payment', '0')
+            ->call('generate')
+            ->assertRedirect();
+
+        $this->assertCount(1, $user->orders);
+        $this->assertCount(2, $user->orders->first()->tickets);
+        $this->assertTrue($user->hasTicketFor($event));
+    }
 }
