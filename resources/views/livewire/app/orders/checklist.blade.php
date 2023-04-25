211d6814cf33d4f6ee3e301d7528fe0102bed058
Andy Newhouse <hi@andymnewhouse.me>
2023-04-25 16:35:09 -0500

    Enlarge text and checkbox

--- resources/views/livewire/app/orders/checklist.blade.php
+++ resources/views/livewire/app/orders/checklist.blade.php
@@ -4,9 +4,9 @@
     @foreach($checklist as $item)
     <div class="relative flex items-start">
       <div class="flex h-6 items-center">
-        <input id="{{ $item['id'] }}" aria-describedby="{{ $item['id'] }}-description" name="{{ $item['id'] }}" type="checkbox" class="h-4 w-4 rounded border-gray-300 dark:border-gray-700 dark:bg-gray-900 text-green-600 focus:ring-green-600">
+        <input id="{{ $item['id'] }}" aria-describedby="{{ $item['id'] }}-description" name="{{ $item['id'] }}" type="checkbox" class="h-5 w-5 rounded border-gray-300 dark:border-gray-700 dark:bg-gray-900 text-green-600 focus:ring-green-600">
       </div>
-      <div class="ml-3 text-sm leading-6">
+      <div class="ml-3 leading-6">
         <label for="{{ $item['id'] }}" class="font-medium text-gray-900">{{ $item['label'] }}</label>
         @isset($item['description'])
         <p id="{{ $item['id'] }}-description" class="text-gray-500">{{ $item['description'] }}</p>
