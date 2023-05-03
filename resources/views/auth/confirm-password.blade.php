4a852c85ba82786fc00f049cf3438f13bcb8ba36
Andy Newhouse <hi@andymnewhouse.me>
2023-05-03 17:47:41 -0500

    Fix tiny logo

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -2,7 +2,7 @@
     <x-auth.card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 text-gray-500 fill-current" />
+                <x-application-logo class="h-12 text-gray-500 fill-current" />
             </a>
         </x-slot>
 
