11be61df9606bf22a88f4d51a7ddddb9c984e6d0
Andy Newhouse <hi@andymnewhouse.me>
2023-05-03 17:40:53 -0500

    Add dark mode for text

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -6,12 +6,12 @@
             </a>
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-200">
             {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 text-sm font-medium text-green-600">
+            <div class="mb-4 text-sm font-medium text-green-600 dark:text-green-200">
                 {{ __('A new verification link has been sent to the email address you provided during registration.') }}
             </div>
         @endif
