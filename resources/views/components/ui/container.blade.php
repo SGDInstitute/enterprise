d6f7d93ef1a286b44c905588a483ad75390f6507
Andy Newhouse <hi@andymnewhouse.me>
2023-05-19 11:15:41 -0500

    Remove extra padding from container

--- resources/views/components/ui/container.blade.php
+++ resources/views/components/ui/container.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'mx-auto max-w-2xl py-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8 lg:py-16']) }}>
+<div {{ $attributes->merge(['class' => 'mx-auto max-w-2xl py-10 px-4 sm:px-0 lg:max-w-7xl lg:py-16']) }}>
     {{ $slot }}
 </div>
