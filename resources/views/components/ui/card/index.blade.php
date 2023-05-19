53e88dc7df959fc798abe6adadfd76bf33687859
Andy Newhouse <hi@andymnewhouse.me>
2023-05-19 11:15:21 -0500

    Remove rounded from card

--- resources/views/components/ui/card/index.blade.php
+++ resources/views/components/ui/card/index.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-800 dark:border dark:border-gray-700 sm:rounded-lg sm:shadow']) }}>
+<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-800 dark:border dark:border-gray-700 sm:shadow']) }}>
     {{ $slot }}
 </div>
