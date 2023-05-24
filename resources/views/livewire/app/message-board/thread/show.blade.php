3415ec66aba2483532008f8c4cd808dc49807691
Andy Newhouse <hi@andymnewhouse.me>
2023-05-24 16:14:27 -0500

    Change from grid to max width

--- resources/views/livewire/app/message-board/thread/show.blade.php
+++ resources/views/livewire/app/message-board/thread/show.blade.php
@@ -23,8 +23,8 @@
             <h1 class="text-xl text-white lg:text-4xl">{{ $thread->title }}</h1>
         </div>
     </section>
-    <x-ui.container  class="grid grid-cols-1 md:grid-cols-4 gap-8">
-        <div class="md:col-span-3">
+    <x-ui.container>
+        <div class="max-w-4xl">
             <x-ui.card >
                 <div class="flex items-center justify-between px-4 py-5 space-x-4 bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700 sm:px-6">
                     <div class="flex items-center space-x-4">
