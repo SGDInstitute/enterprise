37b14af367ac4b17b2e60225175a736826265c59
Andy Newhouse <hi@andymnewhouse.me>
2023-05-25 18:44:50 -0500

    Add response relation manager

---
+++ app/Filament/Resources/FormResource/RelationManagers/ResponsesRelationManager.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources\FormResource\RelationManagers;
+
+use App\Filament\Resources\FormResource;
+use App\Models\Form as ModelsForm;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ResponsesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'responses';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('Proposal ID')
+                    ->searchable(query: fn (Builder $query, string $search): Builder => 
+                        $query->where('responses.id', 'like', "%{$search}%")
+                    )
+                    ->sortable()
+                    ->toggleable(),
+                TextColumn::make('user.name')
+                    ->label('Owner')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+                TextColumn::make('status')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable()
+                    ->wrap(),
+            ])
+            ->filters([
+                SelectFilter::make('status')
+                    ->options([
+                        'work-in-progress' => 'Work in Progress',
+                        'submitted' => 'Submitted',
+                        'in-review' => 'In Review',
+                        'approved' => 'Approved',
+                        'rejected' => 'Rejected',
+                        'scheduled' => 'Scheduled',
+                        'canceled' => 'Canceled',
+                        'confirmed' => 'Confirmed',
+                        'waiting-list' => 'Waiting List',
+                    ])
+                    ->multiple(),
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->actions([
+                Action::make('review')
+                    ->url(fn ($livewire, $record) => FormResource::getUrl('review', ['record' => $livewire->ownerRecord, 'response' => $record]))
+            ])
+            ->bulkActions([
+                DeleteBulkAction::make(),
+            ]);
+    }
+}
