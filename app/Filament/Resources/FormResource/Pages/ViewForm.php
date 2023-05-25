5fae75893f49a0bfeb2d096ff5e23a0d1546a19e
Andy Newhouse <hi@andymnewhouse.me>
2023-05-25 18:22:47 -0500

    Add custom view for Form

--- app/Filament/Resources/FormResource/Pages/ViewForm.php
+++ app/Filament/Resources/FormResource/Pages/ViewForm.php
@@ -3,17 +3,19 @@
 namespace App\Filament\Resources\FormResource\Pages;
 
 use App\Filament\Resources\FormResource;
-use Filament\Pages\Actions;
+use Filament\Pages\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewForm extends ViewRecord
 {
     protected static string $resource = FormResource::class;
 
+    protected static string $view = 'filament.resources.forms.pages.view-form';
+
     protected function getActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            EditAction::make(),
         ];
     }
 }
