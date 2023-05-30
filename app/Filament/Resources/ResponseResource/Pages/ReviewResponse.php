61f87fe6d05f8ab38a246c432db97200a50429c7
Andy Newhouse <hi@andymnewhouse.me>
2023-05-30 17:36:29 -0500

    add review form

--- app/Filament/Resources/ResponseResource/Pages/ReviewResponse.php
+++ app/Filament/Resources/ResponseResource/Pages/ReviewResponse.php
@@ -4,16 +4,30 @@ namespace App\Filament\Resources\ResponseResource\Pages;
 
 use App\Filament\Resources\ResponseResource;
 use App\Models\Response;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\Page;
 use Illuminate\Support\Arr;
 
-class ReviewResponse extends Page
+class ReviewResponse extends Page implements HasForms
 {   
+    use InteractsWithForms;
+
     public Response $record;
 
     protected static string $resource = ResponseResource::class;
 
     protected static string $view = 'filament.resources.response-resource.pages.review';
+
+    protected function getActions(): array
+    {
+        return [
+            //
+        ];
+    }
 
     protected function getViewData(): array
     {
@@ -33,5 +47,48 @@ class ReviewResponse extends Page
                     ? $this->record->answers[$id]
                     : 'was not answered', ];
             });
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            Radio::make('alignment')
+                ->label('Alignment with conference theme & target audience')
+                ->options([
+                    3 => '3: Submission strongly aligns with the intended outcomes of the conference',
+                    2 => '2: Submission generally aligns with the intended outcomes of the conference',
+                    1 => '1: Submission loosely aligns with the intended outcomes of the conference',
+                    0 => '0: Submission did not include information about how it aligns with the theme and target audience',
+                ])
+                ->required(),
+            Radio::make('priority')
+                ->label('Priority of Topic Covered')
+                ->options([
+                    3 => '3: Submission covers a high priority topic',
+                    2 => '2: Submissions covers medium priority topic',
+                    1 => '1: Submission covers a low priority topic',
+                    0 => '0: Submission did not include enough information to score in this category',
+                ])
+                ->required(),
+            Radio::make('presenter_experience')
+                ->label('Appropriateness of presenter covering this content')
+                ->options([
+                    3 => '3: Submission covers a high priority topic',
+                    2 => '2: Submissions covers medium priority topic',
+                    1 => '1: Submission covers a low priority topic',
+                    0 => '0: Submission did not include enough information to score in this category',
+                ])
+                ->required(),
+            Radio::make('track')
+                ->label('Including workshop in a track (only applicable to submissions where track options have been selected)')
+                ->options([
+                    3 => '3: Submission strongly aligns with a selected track description and should be considered for a track slot',
+                    2 => '2: Submission generally aligns with a selected track and could be considered for a track slot as space allows',
+                    1 => '1: Submission loosely aligns with a selected track and would be better suited for a general session',
+                    0 => '0: Submission did not include enough information to score in this category or not tracks were selected',
+                ])
+                ->required(),
+            Textarea::make('notes')->required(),
+        ];
     }
 }
--- resources/views/filament/resources/response-resource/pages/review.blade.php
+++ resources/views/filament/resources/response-resource/pages/review.blade.php
@@ -4,10 +4,10 @@
             <x-filament::card>
                 <x-forms::field-wrapper id="review-form" label="Form" statePath="form.name">
                     {{ $record->form->name }}
-                </x-forms::field-wrapper> 
+                </x-forms::field-wrapper>
                 <x-forms::field-wrapper id="review-type" label="Type" statePath="type">
                     {{ $record->type }}
-                </x-forms::field-wrapper> 
+                </x-forms::field-wrapper>
                 <x-forms::field-wrapper id="review-user-name" label="Creator" statePath="user.name">
                     @isset($record->user)
                     <x-filament-support::link :href="route('filament.resources.users.edit', $record->user)">
@@ -16,7 +16,7 @@
                     @else
                     {{ $record->user->name ?? $record->email ?? 'n/a' }}
                     @endif
-                </x-forms::field-wrapper> 
+                </x-forms::field-wrapper>
                 <x-forms::field-wrapper id="review-co-presenters" label="Co-Presenters" statePath="co-presenters">
                     @forelse($record->collaborators->filter(fn ($user) => $user->id !== $record->user_id) as $collaborator)
                     <div>
@@ -32,28 +32,49 @@
                     @php
                     $emails = $record->invitations->implode('email', ', ');
                     @endphp
-                    
+
                     {{ $emails === '' ? 'no pending invitations' : $emails }}
                 </x-forms::field-wrapper>
                 <x-forms::field-wrapper id="review-status" label="Status" statePath="status">
                     {{ $record->status }}
-                </x-forms::field-wrapper> 
+                </x-forms::field-wrapper>
             </x-filament::card>
             <x-filament::card>
                 @foreach ($qa as $question => $answer)
-                    <div>
-                        <h2 class="mb-2 text-sm text-gray-700 dark:text-gray-400">{{ $question }}</h2>
-                        @if (is_array($answer))
-                            <p class="text-lg text-gray-900 dark:text-gray-200">{{ implode(', ', $answer) }}</p>
-                        @elseif($answer != strip_tags($answer))
-                        <div class="prose dark:prose-light">
-                            {!! $answer !!}
-                        </div>
-                        @else
-                        <p class="text-lg text-gray-900 dark:text-gray-200">{{ $answer }}</p>
-                        @endif
+                <div>
+                    <h2 class="mb-2 text-sm text-gray-700 dark:text-gray-400">{{ $question }}</h2>
+                    @if (is_array($answer))
+                    <p class="text-lg text-gray-900 dark:text-gray-200">{{ implode(', ', $answer) }}</p>
+                    @elseif($answer != strip_tags($answer))
+                    <div class="prose dark:prose-light">
+                        {!! $answer !!}
                     </div>
+                    @else
+                    <p class="text-lg text-gray-900 dark:text-gray-200">{{ $answer }}</p>
+                    @endif
+                </div>
                 @endforeach
+            </x-filament::card>
+        </div>
+        <div>
+            <x-filament::modal width="6xl">
+                <x-slot name="trigger">
+                    <x-filament::button type="button" x-on:click="isOpen = true">View Rubric</x-filament::button>
+                </x-slot>
+
+                <x-slot name="header">
+                    Proposal Review Rubric
+                </x-slot>
+
+                @include('filament.resources.response-resource.actions.rubric')
+            </x-filament::modal>
+
+            <x-filament::card class="mt-4" heading="Review Proposal">
+                <form wire:submit.prevent="submit">
+                    {{ $this->form }}
+                
+                    <x-filament::button type="submit" class="mt-4">Save Review</x-filament::button>
+                </form>
             </x-filament::card>
         </div>
     </div>
