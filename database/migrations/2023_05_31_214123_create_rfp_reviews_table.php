3e66b6871afc10e93c7a29a2f6f28415fa826b25
Andy Newhouse <hi@andymnewhouse.me>
2023-06-03 13:26:26 -0500

    Add columns

--- database/migrations/2023_05_31_214123_create_rfp_reviews_table.php
+++ database/migrations/2023_05_31_214123_create_rfp_reviews_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Form;
+use App\Models\Response;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('rfp_reviews', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('form_id')->constrained();
+            $table->foreignId('response_id')->constrained();
+            $table->tinyInteger('alignment');
+            $table->tinyInteger('experience');
+            $table->tinyInteger('priority');
+            $table->tinyInteger('track');
+            $table->text('notes');
             $table->timestamps();
         });
     }
