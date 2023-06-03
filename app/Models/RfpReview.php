deff261b233739218d4669dceca8cca468180c2c
Andy Newhouse <hi@andymnewhouse.me>
2023-06-03 13:27:08 -0500

    Add relationships

--- app/Models/RfpReview.php
+++ app/Models/RfpReview.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RfpReview extends Model
 {
     use HasFactory;
+
+    public $guarded = [];
+
+    public function form(): BelongsTo
+    {
+        return $this->belongsTo(Form::class);
+    }
+
+    public function response(): BelongsTo
+    {
+        return $this->belingsTo(Response::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
