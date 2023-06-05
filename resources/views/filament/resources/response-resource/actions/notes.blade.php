1fac33fb7ad6342fddc0a671acde2168355cfc80
Andy Newhouse <hi@andymnewhouse.me>
2023-06-05 12:54:51 -0500

    Move scores into a modal

---
+++ resources/views/filament/resources/response-resource/actions/notes.blade.php
@@ -0,0 +1,11 @@
+<div>
+    @foreach($reviews as $review)
+    <div class="flex-auto p-3 rounded-md ring-1 ring-inset ring-gray-200 dark:ring-gray-700">
+        <div class="flex justify-between gap-x-4">
+            <div class="py-0.5 text-xs leading-5 text-gray-500 dark:text-gray-400"><span class="font-medium text-gray-900 dark:text-gray-200">{{ $review->user->name }}</span> wrote</div>
+            <time datetime="{{ $review->created_at }}" class="flex-none py-0.5 text-xs leading-5 text-gray-500 dark:text-gray-400">{{ $review->created_at->diffForHumans() }}</time>
+        </div>
+        <p class="text-sm leading-6 text-gray-500 dark:text-gray-400">{{ $review->notes }}</p>
+    </div>
+    @endforeach
+</div>
--- resources/views/filament/resources/response-resource/pages/review.blade.php
+++ resources/views/filament/resources/response-resource/pages/review.blade.php
@@ -69,42 +69,20 @@
                 @include('filament.resources.response-resource.actions.rubric')
             </x-filament::modal>
 
-            <x-filament::card class="mt-4" heading="Filled Reviews">
-                <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-700">
-                    <thead>
-                        <tr class="divide-x divide-gray-200 dark:divide-gray-700">
-                            <th scope="col"></th>
-                            <th scope="col" class="text-xs -rotate-[50deg] origin-top-left leading-none translate-y-2">Alignment</th>
-                            <th scope="col" class="text-xs -rotate-[50deg] origin-top-left leading-none translate-y-2">Presenter <br>Experience</th>
-                            <th scope="col" class="text-xs -rotate-[50deg] origin-top-left leading-none translate-y-2">Priority</th>
-                            <th scope="col" class="text-xs -rotate-[50deg] origin-top-left leading-none translate-y-2">Track</th>
-                            <th scope="col" class="text-xs -rotate-[50deg] origin-top-left leading-none translate-y-2">Score</th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                        @foreach ($reviews as $review)
-                        <tr class="divide-x divide-gray-200 dark:divide-gray-700">
-                            <td class="p-1 pl-0 text-sm font-medium text-gray-900 dark:text-gray-200 whitespace-nowrap">{{ $review->user->name }}</td>
-                            <td class="p-1 text-sm text-right text-gray-500 dark:text-gray-400 whitespace-nowrap">{{ $review->alignment }}</td>
-                            <td class="p-1 text-sm text-right text-gray-500 dark:text-gray-400 whitespace-nowrap">{{ $review->experience }}</td>
-                            <td class="p-1 text-sm text-right text-gray-500 dark:text-gray-400 whitespace-nowrap">{{ $review->priority }}</td>
-                            <td class="p-1 text-sm text-right text-gray-500 dark:text-gray-400 whitespace-nowrap">{{ $review->track }}</td>
-                            <td class="p-1 text-sm text-right text-gray-500 dark:text-gray-400 whitespace-nowrap">{{ $review->score }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                    <tfoot>
-                        <tr class="divide-x divide-gray-200 dark:divide-gray-700">
-                            <td class="p-1 pl-0 text-sm text-right text-gray-900 dark:text-gray-200 whitespace-nowrap">Total</td>
-                            <td class="p-1 text-sm text-right text-gray-900 dark:text-gray-200 whitespace-nowrap">{{ $reviews->avg('alignment') }}</td>
-                            <td class="p-1 text-sm text-right text-gray-900 dark:text-gray-200 whitespace-nowrap">{{ $reviews->avg('experience') }}</td>
-                            <td class="p-1 text-sm text-right text-gray-900 dark:text-gray-200 whitespace-nowrap">{{ $reviews->avg('priority') }}</td>
-                            <td class="p-1 text-sm text-right text-gray-900 dark:text-gray-200 whitespace-nowrap">{{ $reviews->avg('track') }}</td>
-                            <td class="p-1 text-sm text-right text-gray-900 dark:text-gray-200 whitespace-nowrap">{{ $reviews->avg('score') }}</td>
-                        </tr>
-                    </tfoot>
-                </table>
-            </x-filament::card>
+            <x-filament::modal width="4xl">
+                <x-slot name="trigger">
+                    <x-filament::button type="button" x-on:click="isOpen = true">View Scores & Notes</x-filament::button>
+                </x-slot>
+
+                <x-slot name="header">
+                    Scores & Notes from Reviews
+                </x-slot>
+
+                <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
+                    @include('filament.resources.response-resource.actions.scores')
+                    @include('filament.resources.response-resource.actions.notes')
+                </div>
+            </x-filament::modal>
 
             <x-filament::card class="mt-4" heading="Review this Proposal">
                 <form wire:submit.prevent="submit">
