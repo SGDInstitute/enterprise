adc72f0cf7f1fc90b2b888aadff610c81d7808da
Andy Newhouse <hi@andymnewhouse.me>
2023-06-26 12:53:22 -0500

    Start on notify action

--- app/Filament/Resources/FormResource/Pages/ViewForm.php
+++ app/Filament/Resources/FormResource/Pages/ViewForm.php
@@ -7,6 +7,7 @@ use App\Filament\Resources\FormResource\Widgets\FormatChart;
 use App\Filament\Resources\FormResource\Widgets\SessionChart;
 use App\Filament\Resources\FormResource\Widgets\StatusBreakdown;
 use App\Filament\Resources\FormResource\Widgets\TrackBreakdown;
+use Filament\Pages\Actions\Action;
 use Filament\Pages\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -19,7 +20,23 @@ class ViewForm extends ViewRecord
     protected function getActions(): array
     {
         return [
-            EditAction::make(),
+            Action::make('notify_approved')
+                ->action(fn () => dd('hi'))
+                ->modalHeading('Notify users of Approved Presentations')
+                ->modalSubheading('Are you sure you\'d like to notify these users? This cannot be undone.')
+                ->modalContent(view('filament.resources.form-resource.actions.notification-list', ['status' => 'approved']))
+                ->modalButton('Yes, notify them')
+                ->color('primary'),
+            Action::make('notify_rejected')
+                ->action(fn () => dd('bye'))
+                ->modalHeading('Notify users of Rejected Presentations')
+                ->modalSubheading('Are you sure you\'d like to delete these users? This cannot be undone.')
+                ->modalContent(view('filament.resources.form-resource.actions.notification-list', ['status' => 'rejected']))
+                ->modalButton('Yes, notify them')
+                ->color('danger'),
+            EditAction::make()
+                ->label('Edit Form')
+                ->color('secondary'),
         ];
     }
 
---
+++ resources/views/filament/resources/form-resource/actions/notification-list.blade.php
@@ -0,0 +1,5 @@
+<ul class="ml-4 list-disc">
+    @foreach($this->record->responses()->where('status', $status)->get()->sortBy('name') as $proposal)
+    <li>{{ $proposal->name }} <span class="text-sm">by {{ $proposal->collaborators->implode('name') }}</span></li>
+    @endforeach
+</ul>
