8c2b5de83ad39b7248cda2b7fb73366cfd97f20a
Andy Newhouse <hi@andymnewhouse.me>
2023-09-01 10:36:30 -0500

    Skip test > commenting out

--- tests/Feature/Filament/Resources/FormResource/Pages/ViewFormTest.php
+++ tests/Feature/Filament/Resources/FormResource/Pages/ViewFormTest.php
@@ -15,18 +15,19 @@ class ViewFormTest extends TestCase
     use RefreshDatabase;
 
     // Can't test yet, Need to figure out better way to handle custom columns
-    // /** @test */
-    // public function can_notify_approved_proposals()
-    // {
-    //     $user = User::factory()->create();
-    //     $form = Form::factory()->create(['form' => [
-    //         ['data' => ['id' => 'timeline-information', 'content' => '<h2>Application Timeline</h2>'], 'type' => 'content'],
-    //         ['data' => ['id' => 'name', 'type' => 'text', 'question' => 'Name of Workshop'], 'type' => 'question'],
-    //     ]]);
-    //     $proposal = Response::factory()->for($form)->for($user)->create(['answers' => ['name' => 'Hello world']]);
+    /** @test */
+    public function can_notify_approved_proposals()
+    {
+        $this->markTestSkipped();
+        $user = User::factory()->create();
+        $form = Form::factory()->create(['form' => [
+            ['data' => ['id' => 'timeline-information', 'content' => '<h2>Application Timeline</h2>'], 'type' => 'content'],
+            ['data' => ['id' => 'name', 'type' => 'text', 'question' => 'Name of Workshop'], 'type' => 'question'],
+        ]]);
+        $proposal = Response::factory()->for($form)->for($user)->create(['answers' => ['name' => 'Hello world']]);
 
-    //     Livewire::actingAs($user)
-    //         ->test(ViewForm::class, ['record' => $form->id])
-    //         ->callPageAction('notify_approved');
-    // }
+        Livewire::actingAs($user)
+            ->test(ViewForm::class, ['record' => $form->id])
+            ->callPageAction('notify_approved');
+    }
 }
--- tests/Feature/Filament/Resources/FormResource/RelationManagers/ResponsesRelationManagerTest.php
+++ tests/Feature/Filament/Resources/FormResource/RelationManagers/ResponsesRelationManagerTest.php
@@ -15,20 +15,21 @@ class ResponsesRelationManagerTest extends TestCase
     use RefreshDatabase;
 
     // Can't test yet, Need to figure out better way to handle custom columns
-    // /** @test */
-    // public function can_bulk_change_status()
-    // {
-    //     $user = User::factory()->create();
-    //     $form = Form::factory()->create(['form' => [
-    //         ['data' => ['id' => 'timeline-information', 'content' => '<h2>Application Timeline</h2>'], 'type' => 'content'],
-    //         ['data' => ['id' => 'name', 'type' => 'text', 'question' => 'Name of Workshop'], 'type' => 'question'],
-    //     ]]);
-    //     $acceptedProposals = Response::factory()->for($form)->count(5)->create(['answers' => ['name' => 'Hello world', 'status' => 'submitted']]);
-    //     $deniedProposals = Response::factory()->for($form)->count(5)->create(['answers' => ['name' => 'Foo Bar'], 'status' => 'submitted']);
+    /** @test */
+    public function can_bulk_change_status()
+    {
+        $this->markTestSkipped();
+        $user = User::factory()->create();
+        $form = Form::factory()->create(['form' => [
+            ['data' => ['id' => 'timeline-information', 'content' => '<h2>Application Timeline</h2>'], 'type' => 'content'],
+            ['data' => ['id' => 'name', 'type' => 'text', 'question' => 'Name of Workshop'], 'type' => 'question'],
+        ]]);
+        $acceptedProposals = Response::factory()->for($form)->count(5)->create(['answers' => ['name' => 'Hello world', 'status' => 'submitted']]);
+        $deniedProposals = Response::factory()->for($form)->count(5)->create(['answers' => ['name' => 'Foo Bar'], 'status' => 'submitted']);
 
-    //     Livewire::test(ResponsesRelationManager::class, ['ownerRecord' => $form])
-    //         ->assertCanSeeTableRecords($acceptedProposals)
-    //         ->assertCanSeeTableRecords($deniedProposals)
-    //         ->callTableBulkAction('change_status', $acceptedProposals, ['status' => 'accepted']);
-    // }
+        Livewire::test(ResponsesRelationManager::class, ['ownerRecord' => $form])
+            ->assertCanSeeTableRecords($acceptedProposals)
+            ->assertCanSeeTableRecords($deniedProposals)
+            ->callTableBulkAction('change_status', $acceptedProposals, ['status' => 'accepted']);
+    }
 }
