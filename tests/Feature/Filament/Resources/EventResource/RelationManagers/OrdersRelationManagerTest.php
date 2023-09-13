f4f5f961eafe73ba6fcf5eee73550df0759fef6b
Andy Newhouse <hi@andymnewhouse.me>
2023-09-13 13:50:55 -0500

    Fix implementation, add new notification, add tests

--- tests/Feature/Filament/Resources/EventResource/RelationManagers/OrdersRelationManagerTest.php
+++ tests/Feature/Filament/Resources/EventResource/RelationManagers/OrdersRelationManagerTest.php
@@ -2,19 +2,18 @@
 
 namespace Tests\Feature\Filament\Resources\ResponseResource\Pages;
 
+use App\Filament\Actions\MarkAsUnpaidAction;
+use App\Filament\Actions\RefundAction;
 use App\Filament\Resources\EventResource\Pages\EditEvent;
-use App\Filament\Resources\EventResource\RelationManagers\EventItemsRelationManager;
 use App\Filament\Resources\EventResource\RelationManagers\OrdersRelationManager;
 use App\Models\Event;
-use App\Models\EventItem;
 use App\Models\Order;
-use App\Models\Response;
 use App\Models\Ticket;
-use App\Models\User;
-use App\Notifications\WorkshopScheduled;
+use App\Notifications\Refund;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
 use Livewire\Livewire;
+use PHPUnit\Framework\Test;
 use Tests\TestCase;
 
 class OrdersRelationManagerTest extends TestCase
@@ -38,7 +37,7 @@ class OrdersRelationManagerTest extends TestCase
     public function can_only_view_paid_orders(): void
     {
         $event = Event::factory()->create();
-        $order = Order::factory()->for($event)-> paid()->create();
+        $order = Order::factory()->for($event)->paid()->create();
         $reservation = Order::factory()->for($event)->create();
 
         Livewire::test(OrdersRelationManager::class, [
@@ -47,5 +46,89 @@ class OrdersRelationManagerTest extends TestCase
         ])
             ->assertCanSeeTableRecords([$order])
             ->assertCanNotSeeTableRecords([$reservation]);
+    }
+
+    /** @test */
+    public function can_mark_order_as_unpaid(): void
+    {
+        $event = Event::factory()->create();
+        $order = Order::factory()->for($event)->paid()->create();
+
+        Livewire::test(OrdersRelationManager::class, [
+            'ownerRecord' => $event,
+            'pageClass' => EditEvent::class,
+        ])
+            ->callTableAction(MarkAsUnpaidAction::class, $order)
+            ->assertNotified();
+
+        $this->assertNull($order->fresh()->transaction_id);
+    }
+
+    /** @test */
+    public function cannot_mark_order_as_unpaid_if_processed_through_stripe(): void
+    {
+        $event = Event::factory()->create();
+        $order = Order::factory()->for($event)->paid()->create([
+            'transaction_id' => 'pi_test_1234',
+        ]);
+
+        Livewire::test(OrdersRelationManager::class, [
+            'ownerRecord' => $event,
+            'pageClass' => EditEvent::class,
+        ])
+            ->assertTableActionDisabled(MarkAsUnpaidAction::class, $order);
+
+        $this->assertNotNull($order->fresh()->transaction_id);
+    }
+
+    /** @test */
+    public function can_refund_ticket_on_order(): void
+    {
+        Notification::fake();
+
+        $event = Event::factory()->create();
+        $order = Order::factory()->for($event)->paid()->create([
+            'amount' => 2000
+        ]);
+        [$ticketA, $ticketB] = Ticket::factory()->for($order)->for($event)->count(2)->create();
+
+        Livewire::test(OrdersRelationManager::class, [
+            'ownerRecord' => $event,
+            'pageClass' => EditEvent::class,
+        ])->callTableAction(RefundAction::class, $order, data: [
+            $ticketA->id => true,
+        ]);
+
+        $this->assertSoftDeleted($ticketA);
+        $this->assertNotSoftDeleted($ticketB);
+        $this->assertEquals(1000, $order->fresh()->amount);
+
+        Notification::assertSentTo($order->user, Refund::class);
+    }
+
+    /** @test */
+    public function can_refund_entire_order(): void
+    {
+        Notification::fake();
+
+        $event = Event::factory()->create();
+        $order = Order::factory()->for($event)->paid()->create([
+            'amount' => 2000
+        ]);
+        [$ticketA, $ticketB] = Ticket::factory()->for($order)->for($event)->count(2)->create();
+
+        Livewire::test(OrdersRelationManager::class, [
+            'ownerRecord' => $event,
+            'pageClass' => EditEvent::class,
+        ])->callTableAction(RefundAction::class, $order, data: [
+            $ticketA->id => true,
+            $ticketB->id => true,
+        ]);
+
+        $this->assertSoftDeleted($ticketA);
+        $this->assertSoftDeleted($ticketB);
+        $this->assertSoftDeleted($order);
+
+        Notification::assertSentTo($order->user, Refund::class);
     }
 }
