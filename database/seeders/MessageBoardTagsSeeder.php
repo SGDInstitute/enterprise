92824c0c10dea0b3df3963344f952a88127f5472
Andy Newhouse <hi@andymnewhouse.me>
2023-09-14 17:19:42 -0500

    Add tags

---
+++ database/seeders/MessageBoardTagsSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Tags\Tag;
+
+class MessageBoardTagsSeeder extends Seeder
+{
+    public $states = [
+        'North Dakota',
+        'South Dakota',
+        'Nebraska',
+        'Kansas',
+        'Minnesota',
+        'Iowa',
+        'Missouri',
+        'Wisconsin',
+        'Illinois',
+        'Michigan',
+        'Michigan (UP)',
+        'Indiana',
+        'Kentucky',
+        'Ohio',
+    ];
+
+    public $types = [
+        'Travel',
+        'Lodging',
+    ];
+
+    public function run(): void
+    {
+        foreach ($this->types as $type) {
+            Tag::findOrCreate($type, 'posts');
+        }
+
+        foreach ($this->states as $state) {
+            Tag::findOrCreate($state, 'posts');
+        }
+    }
+}
