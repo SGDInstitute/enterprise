53be76e98845b764af1b35bd534a6d682f13ebfd
Andy Newhouse <hi@andymnewhouse.me>
2023-09-15 17:15:51 -0500

    Start on slack notifications

---
+++ app/Notifications/PostCreated.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Event;
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PostCreated extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(public Event $event, public Post $post)
+    {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['slack'];
+    }
+
+    public function toSlack(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
