1dd8349c865beee8b8638f7d0ae19e54944ac321
Andy Newhouse <andy@sgdinstitute.org>
2023-09-26 18:51:52 -0500

    Fix health check email

--- config/health.php
+++ config/health.php
@@ -58,7 +58,7 @@ return [
         'throttle_notifications_key' => 'health:latestNotificationSentAt:',
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => 'andy@sgdinstitute.org',
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
