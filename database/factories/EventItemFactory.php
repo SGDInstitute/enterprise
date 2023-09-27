ae9824c46a78a0fd18970c8cab0b6751a9481627
Andy Newhouse <andy@sgdinstitute.org>
2023-09-27 09:25:19 -0500

    Add relation and type hinting

--- database/factories/EventItemFactory.php
+++ database/factories/EventItemFactory.php
@@ -9,7 +9,10 @@ class EventItemFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'start' => now(),
+            'end' => now()->addMinutes(5),
+            'timezone' => 'America/Chicago',
         ];
     }
 }
