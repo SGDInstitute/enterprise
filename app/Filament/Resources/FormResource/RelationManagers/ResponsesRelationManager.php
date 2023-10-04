b0a735cbb3f5c96a96a617e6b7b53a7bc22b9e17
Andy Newhouse <andy@sgdinstitute.org>
2023-10-04 09:23:39 -0500

    JKS

--- app/Filament/Resources/FormResource/RelationManagers/ResponsesRelationManager.php
+++ app/Filament/Resources/FormResource/RelationManagers/ResponsesRelationManager.php
@@ -8,6 +8,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
@@ -24,16 +25,6 @@ class ResponsesRelationManager extends RelationManager
     protected static string $relationship = 'responses';
 
     protected static ?string $recordTitleAttribute = 'name';
-
-    public function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-            ]);
-    }
 
     public function table(Table $table): Table
     {
@@ -129,8 +120,12 @@ class ResponsesRelationManager extends RelationManager
                     )),
             ], layout: FiltersLayout::AboveContent)
             ->actions([
-                ViewAction::make()
-                    ->url(fn ($record) => ResponseResource::getUrl('view', ['record' => $record])),
+                ActionGroup::make([
+                    ViewAction::make()
+                        ->url(fn ($record) => ResponseResource::getUrl('view', ['record' => $record])),
+                    // Action::make('create_order')
+                    //     ->hidden(fn ($record) => in_array($record->status, ['confirmed', 'scheduled']))
+                ]),
             ], position: ActionsPosition::BeforeColumns)
             ->bulkActions([
                 BulkAction::make('change_status')
@@ -153,6 +148,19 @@ class ResponsesRelationManager extends RelationManager
                             ])
                             ->required(),
                     ])
+                    ->deselectRecordsAfterCompletion(),
+                BulkAction::make('create_orders')
+                    ->action(function (Collection $records) {
+                        $records = $records->filter(fn ($record) => in_array($record->status, ['scheduled', 'confirmed']));
+
+                        $collaborators = $records->flatMap->collaborators->unique();
+
+                        // filter out proposals with invitations
+                            // send out reminder that we can't create order w/ pending invitations
+                        // create comped orders for user & all collaborators
+                            // send out to users notification that order was created
+                        // send toast notification that action is done
+                    })
                     ->deselectRecordsAfterCompletion(),
             ])
             ->persistFiltersInSession()
---
+++ tests/Feature/Filament/Resources/FormResource/RelationManagers/ResponsesRelationManagerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Filament\Resources\FormResource\RelationManagers;
+
+use App\Filament\Resources\FormResource\Pages\ViewForm;
+use App\Filament\Resources\FormResource\RelationManagers\ResponsesRelationManager;
+use App\Models\Form;
+use App\Models\Order;
+use App\Models\Response;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+final class ResponsesRelationManagerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function can_render_responses_for_form()
+    {
+        $schema = json_decode(file_get_contents(base_path('tests/Feature/Filament/Resources/FormResource/RelationManagers/proposal-form.json')), true);
+        $form = Form::factory()->create(['form' => $schema]);
+        $responses = Response::factory()->for($form)->count(5)->create(['answers' => ['name' => 'Hello world', 'format' => 'presentation', 'session' => ['breakout-1'], 'track-first-choice' => 'rural', 'track-second-choice' => 'rural']]);
+
+        Livewire::test(ResponsesRelationManager::class, ['ownerRecord' => $form,'pageClass' => ViewForm::class])
+            ->assertSuccessful()
+            ->assertCanSeeTableRecords($responses);
+    }
+
+    #[Test]
+    public function can_make_orders_in_bulk()
+    {
+        $schema = json_decode(file_get_contents(base_path('tests/Feature/Filament/Resources/FormResource/RelationManagers/proposal-form.json')), true);
+        $form = Form::factory()->create(['form' => $schema]);
+        $responses = Response::factory()->for($form)
+            ->count(5)
+            ->state(new Sequence(
+                ['status' => 'work-in-progress'],
+                ['status' => 'rejected'],
+                ['status' => 'canceled'],
+            ))
+            ->has(User::factory()->count(2), 'collaborators')
+            ->create(['answers' => ['name' => 'Hello world', 'format' => 'presentation', 'session' => ['breakout-1'], 'track-first-choice' => 'rural', 'track-second-choice' => 'rural']]);
+        [$scheduled, $confirmed] = Response::factory()->for($form)
+            ->count(2)
+            ->state(new Sequence(
+                ['status' => 'scheduled'],
+                ['status' => 'confirmed'],
+            ))
+            ->has(User::factory()->count(2), 'collaborators')
+            ->create(['answers' => ['name' => 'Hello world', 'format' => 'presentation', 'session' => ['breakout-1'], 'track-first-choice' => 'rural', 'track-second-choice' => 'rural']]);
+
+        Livewire::test(ResponsesRelationManager::class, ['ownerRecord' => $form,'pageClass' => ViewForm::class])
+            ->callTableBulkAction('create_orders', [...$responses, $scheduled, $confirmed]);
+
+        $this->assertCount(4, Order::all());
+    }
+}
