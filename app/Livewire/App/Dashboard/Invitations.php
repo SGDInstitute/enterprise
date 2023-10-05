c80b235db8a62a89d2a19f5cafd8375a851c1b4e
Andy Newhouse <andy@sgdinstitute.org>
2023-10-05 16:30:02 -0500

    Add invitations page

---
+++ app/Livewire/App/Dashboard/Invitations.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire\App\Dashboard;
+
+use App\Livewire\Traits\WithFiltering;
+use App\Livewire\Traits\WithSorting;
+use App\Models\Invitation;
+use App\Models\Order;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Invitations extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.dashboard.invitations')
+            ->with([
+                'invitations' => $this->invitations,
+            ]);
+    }
+
+    public function getInvitationsProperty()
+    {
+        return Invitation::where('email', auth()->user()->email)->get();
+    }
+}
--- resources/views/livewire/app/dashboard.blade.php
+++ resources/views/livewire/app/dashboard.blade.php
@@ -1,9 +1,10 @@
 <div class="container px-4 pt-4 pb-12 mx-auto md:py-12 md:px-12">
     <div class="grid grid-cols-1 gap-8 md:grid-cols-4">
         <nav class="space-y-1" aria-label="Sidebar">
-            @foreach (config('nav.app.dashboard') as $link)
-                <x-galaxy.nav-link :href="route($link['route'], $link['route-param'])" :icon="$link['icon']" :active="$link['route-param'] === $page" >{{ $link['name'] }}</x-galaxy.nav-link>
-            @endforeach
+            @if (auth()->user()->has_invitations)
+                <x-galaxy.nav-link :href="route('app.dashboard', 'invitations')" icon="heroicon-o-document" active="'invitations' === $page" >Invitations</x-galaxy.nav-link>
+            @endif
+
             <x-galaxy.nav-link :href="route('app.dashboard', 'orders-reservations')" icon="heroicon-o-calendar" :active="'orders-reservations' === $page" >Event Tickets</x-galaxy.nav-link>
             <x-galaxy.nav-link :href="route('app.dashboard', 'workshops')" icon="heroicon-o-light-bulb" :active="'workshops' === $page" >Workshop Submissions</x-galaxy.nav-link>
             <x-galaxy.nav-link :href="route('app.dashboard', 'donations')" icon="heroicon-o-gift" :active="'donations' === $page" >Donations</x-galaxy.nav-link>
@@ -19,6 +20,8 @@
             <livewire:app.dashboard.donations />
             @elseif ($page === 'settings')
             <livewire:app.dashboard.settings />
+            @elseif ($page === 'invitations')
+            <livewire:app.dashboard.invitations />
             @endif
         </div>
     </div>
