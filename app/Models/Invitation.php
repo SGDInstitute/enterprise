bcf979b3bb5f69799e4ed92348b014614d85ae69
Andy Newhouse <andy@sgdinstitute.org>
2023-10-09 20:24:25 -0500

    Automatically create ticket for folks accepting invitation

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Notifications\InvitationAccepted;
+use Facades\App\Actions\GenerateCompedOrder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -47,16 +48,34 @@ class Invitation extends Model
     {
         $url = $this->redirectUrl;
         if ($this->inviteable_type === Response::class) {
-            $response = $this->inviteable;
-            $response->collaborators()->attach(auth()->user());
+            $this->handleResponse($this->inviteable);
         } elseif ($this->inviteable_type === Ticket::class) {
-            $ticket = $this->inviteable;
-            $ticket->update(['user_id' => auth()->id()]);
+            $this->handleTicket($this->inviteable);
         }
 
         $this->inviter->notify(new InvitationAccepted($this->inviteable, auth()->user()));
         $this->delete();
 
         return redirect($url);
+    }
+
+    private function handleResponse(Response $response)
+    {
+        $response = $this->inviteable;
+        $response->collaborators()->attach(auth()->user());
+
+        if (in_array($response->status, ['confirmed', 'scheduled'])) {
+            GenerateCompedOrder::presenter(
+                $response->form->event,
+                $response,
+                auth()->user(),
+            );
+        }
+    }
+
+    private function handleTicket(Ticket $ticket)
+    {
+        $ticket = $this->inviteable;
+        $ticket->update(['user_id' => auth()->id()]);
     }
 }
