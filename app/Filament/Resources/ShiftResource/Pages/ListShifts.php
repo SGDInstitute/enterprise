2bc0678f9b114ba0e61eed22399a223e1c182553
Andy Newhouse <andy@sgdinstitute.org>
2023-10-11 20:10:23 -0500

    Add action to view frontend

--- app/Filament/Resources/ShiftResource/Pages/ListShifts.php
+++ app/Filament/Resources/ShiftResource/Pages/ListShifts.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\ShiftResource\Pages;
 
 use App\Filament\Resources\ShiftResource;
+use App\Models\Event;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Forms\Components\Select;
 use Filament\Resources\Pages\ListRecords;
 
 class ListShifts extends ListRecords
@@ -13,6 +16,14 @@ class ListShifts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('view_frontend')
+                ->modalWidth('sm')
+                ->form([
+                    Select::make('event')
+                        ->options(Event::whereHas('shifts')->pluck('name', 'slug')),
+                ])
+                ->outlined()
+                ->action(fn ($data) => redirect()->route('app.volunteer', $data['event'])),
             Actions\CreateAction::make(),
         ];
     }
