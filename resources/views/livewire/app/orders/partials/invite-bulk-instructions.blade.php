6690713b8438a7c997fb8b8ebc8135f57a061ff1
Andy Newhouse <andy@sgdinstitute.org>
2023-10-20 15:44:56 -0500

    Add more info to instructions

--- resources/views/livewire/app/orders/partials/invite-bulk-instructions.blade.php
+++ resources/views/livewire/app/orders/partials/invite-bulk-instructions.blade.php
@@ -2,4 +2,5 @@
     <p>There are currently {{ $count }} unassigned tickets. Fill in the textarea below with emails of those you would like to fill those tickets.</p>
     <p>After clicking submit, notifications will be sent to each email provided asking them to "Accept Invite"</p>
     <p>When clicking the "Accept Invite" users who do not exist in our system will be asked to create an account and users who do exist will be asked to login.</p>
+    <p>Once creating an account or logging in, the user will be directed to an attendee page, where they can update their name, pronouns, dietary restrictions, accessability requests and demographic information.</p>
 </div>
