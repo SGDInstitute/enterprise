0dbc90e43b4872bcbcdbe8ce9ac0b30b1127df2c
Andy Newhouse <andy@sgdinstitute.org>
2023-10-22 09:38:37 -0500

    Update title for reports page

--- app/Filament/Resources/EventResource/Pages/ReportEvent.php
+++ app/Filament/Resources/EventResource/Pages/ReportEvent.php
@@ -3,11 +3,20 @@
 namespace App\Filament\Resources\EventResource\Pages;
 
 use App\Filament\Resources\EventResource;
+use App\Models\Event;
 use Filament\Resources\Pages\Page;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ReportEvent extends Page
 {
     protected static string $resource = EventResource::class;
 
     protected static string $view = 'filament.resources.event-resource.pages.report-event';
+
+    public Event $record;
+
+    public function getTitle(): string | Htmlable
+    {
+        return $this->record->name . ' Reports';
+    }
 }
