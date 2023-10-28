6a009e4b384d25dbe17aaf54b113be93d7cf63d6
Andy Newhouse <andy@sgdinstitute.org>
2023-10-28 14:45:03 -0500

    Update payment reminder text

--- app/Notifications/PaymentReminder.php
+++ app/Notifications/PaymentReminder.php
@@ -28,7 +28,7 @@ class PaymentReminder extends Notification implements ShouldQueue
             ->line("This is a reminder that you have an outstanding reservation for {$this->order->tickets_count} tickets to {$this->order->event->name}.")
             ->line('Orders can be paid by credit card or by check')
             ->line("If paying by card, once the payment is successful, you'll receive a receipt and confirmation that your order is paid. If you have any issues with your credit card payment, please contact us and we'll do our best to troubleshoot.")
-            ->line('If paying by check, please make them payable to "Midwest Institute for Sexuality and Gender Diversity" and mailed to P.O. BOX 1053, East Lansing, MI 48826-1053. As soon as we receive your check, your order will be marked as paid.')
+            ->line('If paying by check, please make it payable to "Midwest Institute for Sexuality and Gender Diversity" and mailed to P.O. BOX 1053, East Lansing, MI 48826-1053. As soon as we receive your check, your order will be marked as paid. Mail must be received by Oct 31 or you can pay with check on site.')
             ->action('Pay Now', route('app.orders.show', ['order' => $this->order]));
     }
 
