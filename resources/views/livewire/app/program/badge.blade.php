8c3cc0f614856d397403007e21519bbf1d4c6d74
Andy Newhouse <andy@sgdinstitute.org>
2023-11-03 09:24:25 -0400

    Pass event to to name badge

--- resources/views/livewire/app/program/badge.blade.php
+++ resources/views/livewire/app/program/badge.blade.php
@@ -1,7 +1,7 @@
-<div class="space-y-4">
+<div class="space-y-4 max-w-prose mx-auto">
     <div class="prose dark:prose-light">
         <h1>Badge</h1>
     </div>
 
-    <x-name-badge :user="$user" />
+    <x-name-badge :user=$user :event=$event />
 </div>
