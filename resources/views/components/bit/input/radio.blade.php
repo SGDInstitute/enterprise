960b80343d3b1861397ec050cb67dfcabb7cf006
Andy Newhouse <andy@sgdinstitute.org>
2023-11-29 11:46:41 -0600

    Fix radio buttons in dark mode

--- resources/views/components/bit/input/radio.blade.php
+++ resources/views/components/bit/input/radio.blade.php
@@ -4,7 +4,7 @@
 
 @if ($label)
 <div class="flex items-center">
-    <input {{ $attributes }} type="radio" class="w-4 h-4 dark:bg-gray-800 text-green-600 transition duration-150 ease-in-out form-radio focus:ring-green-500 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}">
+    <input {{ $attributes }} type="radio" class="h-4 w-4 border-gray-300 bg-transparent dark:border-gray-700 text-green-600 focus:ring-green-600 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}">
     <label for="{{ $attributes['id'] }}" class="ml-3">
         <span class="block text-sm font-medium leading-5 text-gray-700 dark:text-gray-200 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}">{{ $label }}</span>
     </label>
@@ -13,7 +13,7 @@
 <div {{ $attributes->only('class') }}>
     <input {{ $attributes->except('class') }}
         type="radio"
-        class="block transition dark:bg-gray-800 duration-150 ease-in-out text-green-600 border-gray-300 form-radio focus:ring-green-500 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}"
+        class="h-4 w-4 border-gray-300 bg-transparent dark:border-gray-700 text-green-600 focus:ring-green-600 {{ $attributes->get('disabled') ? 'opacity-75 cursor-not-allowed' : '' }}"
     />
 </div>
 @endif
