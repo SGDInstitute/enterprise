9c3a9fe555a29d79c4c0da6a474daf76742fe7b4
Shift <shift@laravelshift.com>
2024-07-29 15:05:50 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\FilamentServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\ViewServiceProvider::class,
 ];
