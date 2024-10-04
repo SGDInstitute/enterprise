3242d9d5a154e5c3c367a094c88acdb4547cd27b
Andy Newhouse <andy@sgdinstitute.org>
2024-10-03 21:15:36 -0500

    Fix tests

--- app/Http/Middleware/HasTicketForEvent.php
+++ app/Http/Middleware/HasTicketForEvent.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Event;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +10,7 @@ class HasTicketForEvent
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $event = Event::where('slug', $request->route('event'))->firstOrFail();
+        $event = $request->route('event');
 
         abort_if(! $request->user()->hasTicketFor($event), 403, "You must have a ticket for {$event->name} to view the message board.");
 
